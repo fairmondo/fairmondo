224de6509b1ea42d19bf609f15c84a3f520ef05e
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-04-19 20:31:14 +0200

    Formtastic login

--- fairnopoly/app/helpers/application_helper.rb
+++ fairnopoly/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # Map Flash to Bootstrap CSS
+  def bootstrap_notice_mapper(type)
+    case type
+    when :alert
+      "warning"
+    when :error
+      "error"
+    when :notice
+      "success"
+    else
+    "info"
+    end
+  end
 end
