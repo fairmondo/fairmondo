cb035a2382de2f872639cbd6f21f255e9b04e3a9
Tobias Schmid <2toschmid@googlemail.com>
2012-04-26 11:58:07 +0200

    Dashboard

--- fairnopoly/app/controllers/dashboard_controller.rb
+++ fairnopoly/app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
+  
+  # GET /auctions
+  # GET /auctions.json
   def index
+       @auctions = Auction.all
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render :json => @auctions }
+      end
   end
 end
