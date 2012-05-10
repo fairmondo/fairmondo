59db7d2cd94a6b8da0f74e0fcd4db2c9cdc72a90
Tobias Schmid <2toschmid@googlemail.com>
2012-05-10 22:55:46 +0200

    pagination

--- fairnopoly/app/controllers/dashboard_controller.rb
+++ fairnopoly/app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
   # GET /dashboard.json
   def index
     
-    @userevents = Userevent.find(:all,:conditions => [ "user_id = ?", current_user.id], :order =>"created_at DESC")
+    @userevents = Userevent.where(:user_id => current_user.id).paginate(:page => params[:page],:per_page => 2).order('created_at DESC') #find(:all,:conditions => [ "user_id = ?", current_user.id], :order =>"created_at DESC")
     
       respond_to do |format|
         format.html # index.html.erb
--- fairnopoly/app/models/userevent.rb
+++ fairnopoly/app/models/userevent.rb
@@ -2,6 +2,6 @@ class Userevent < ActiveRecord::Base
   belongs_to :user
   belongs_to :appended_object, :polymorphic => true
   validates_presence_of :user, :event_type
-  
+
   
 end
