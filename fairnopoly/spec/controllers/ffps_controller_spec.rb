c0c1ab493dc98de11831034ac28ab57ff311cb3f
Sebastian Sucker <sebastian@agile-workers.com>
2012-08-07 14:39:26 +0200

    Completed ffps_controller tests.

--- fairnopoly/spec/controllers/ffps_controller_spec.rb
+++ fairnopoly/spec/controllers/ffps_controller_spec.rb
@@ -52,4 +52,137 @@ describe FfpsController do
     end  
   end
   
+  describe "GET 'new'" do
+
+    describe "for non-signed-in users" do 
+
+      it "should deny access" do
+        get :new
+        response.should redirect_to('/users/sign_in')
+      end
+    end
+
+    describe "for signed-in users" do
+
+      before :each do
+        @user = FactoryGirl.create(:user)
+        sign_in @user
+      end
+
+      it "it should be successful" do
+        get :new
+        response.should be_success
+      end
+    end
+  end
+
+  describe "POST 'create'" do
+
+    describe "for non-signed-in users" do 
+
+      before :each do
+        @goodPrice = { :price => Random.new.rand(10..500)}
+      end
+
+        it "should deny access" do
+          get :create
+          response.should redirect_to('/users/sign_in')
+        end
+
+        it "should not create a ffp" do
+          lambda do
+            post :create, :ffp => @goodPrice
+          end.should_not change(Ffp, :count)
+        end
+    end
+
+    describe "for signed-in users" do
+
+      before :each do
+        @user = FactoryGirl.create(:user)
+        @goodPrice  = { :price => Random.new.rand(10..500)}
+        @lowPrice   = { :price => Random.new.rand(0..9)}
+        @highPrice  = { :price => Random.new.rand(500..9999999)}
+        sign_in @user
+      end
+
+      it "should create a ffp" do
+        lambda do
+          post :create, :ffp => @goodPrice
+        end.should change(Ffp, :count).by(1)
+      end
+
+      it "should not create a ffp (price low)" do
+        lambda do
+          post :create, :ffp => @lowPrice
+        end.should_not change(Ffp, :count)
+      end
+
+      it "should not create a ffp (price high)" do
+        lambda do
+          post :create, :ffp => @highPrice
+        end.should_not change(Ffp, :count)
+      end
+
+    end 
+  end
+
+  describe "PUT 'update'" do
+
+    describe "for non-signed-in users" do 
+
+      it "should deny access" do
+        put :update
+        response.should redirect_to('/users/sign_in')
+      end
+    end
+
+    describe "for signed-in users" do
+
+      before :each do
+        @user = FactoryGirl.create(:user)
+        @ffp  = FactoryGirl.create(:ffp)
+        sign_in @user
+      end
+
+      it "it should be successful" do
+        put :update, id: @ffp
+        response.should redirect_to(ffps_path)
+      end
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+
+    describe "for non-signed-in users" do 
+
+      it "should deny access" do
+        delete :destroy
+        response.should redirect_to('/users/sign_in')
+      end
+
+      it "should not delete ffp" do
+        lambda do
+          delete :destroy
+        end.should_not change(Ffp, :count)
+      end
+    end
+
+    describe "for signed-in users" do
+
+      before :each do
+        @user = FactoryGirl.create(:user)
+        @ffp  = FactoryGirl.create(:ffp)
+        sign_in @user
+      end
+
+      it "should create a ffp" do
+        lambda do
+          delete :destroy, :id => @ffp
+        end.should change(Ffp, :count).by(-1)
+      end
+
+    end
+  end
+
 end
