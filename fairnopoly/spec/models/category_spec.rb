9df3d7232d93afaf6d17286be841d838ac39ca71
Sebastian Sucker <sebastian@agile-workers.com>
2012-08-20 13:36:56 +0200

    Extended category factory

--- fairnopoly/spec/models/category_spec.rb
+++ fairnopoly/spec/models/category_spec.rb
@@ -8,5 +8,10 @@ describe Category do
   
   let(:category) { FactoryGirl::create(:category)}
   
+  it "should have the correct parent_id" do
+    @category = FactoryGirl.create(:category)
+    @anotherCategory = FactoryGirl.create(:category, :parent_id => @category)
+    @anotherCategory.parent.should eq @category
+  end
   it {should have_many :auctions}
 end
