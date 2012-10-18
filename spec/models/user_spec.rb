b3157436a027f27bdd1aba70b724b5ab60034481
Sebastian Sucker <sebastian@agile-workers.com>
2012-10-18 17:25:33 +0200

    Fixed user spec & deleted unused spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  
+
   it "has a valid Factory" do
     FactoryGirl.create(:user).should be_valid
   end
-  
+
   let(:user) { FactoryGirl::create(:user)}
-  
+
   it {should validate_presence_of :email}
-  
+
   it {should have_many :auctions}
   it {should have_many :userevents}
   it {should have_many :bids}
   it {should have_many :invitations}
+
+  it "return correct fullname" do
+    user = FactoryGirl.create(:user)
+    user.fullname.should == "#{user.name} #{user.surname}"
+  end
+
+  it "returns correct display_name" do
+    user = FactoryGirl.create(:user)
+    user.display_name.should == user.fullname
+  end
+
 end
