8a45c8c87bcbe5ba1d7288b63221d3fb5a6aeadc
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-10-27 01:18:26 +0200

    Deploy Hook Reloaded

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,1 +1,1 @@
-run! "ln -nfs #{shared_path}/config/production.rb #{release_path}/config/environments/production.rb"
+exec "ln -nfs #{shared_path}/config/production.rb #{release_path}/config/environments/production.rb"
