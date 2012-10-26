23db9035271261b9299a7e318d96f0d3207eb15c
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-10-27 01:02:20 +0200

    Deploy Hook Reloaded

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,2 +1,1 @@
-run "cp #{shared_path}/config/production.rb #{release_path}/config/environments/production.rb"
-
+run! "ln -nfs #{shared_path}/config/production.rb #{release_path}/config/environments/production.rb"
