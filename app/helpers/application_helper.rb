b2e4d2e3847551fadb24e14446bfff6f342d7ffc
Tobias Schmid <2toschmid@googlemail.com>
2012-10-30 17:34:56 +0100

    bug fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,17 +56,6 @@ module ApplicationHelper
      new_param
   end
    
-  def resource_name
-    :user
-  end
  
-  def resource
-    @resource ||= User.new
-  end
- 
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-   
    
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,3 +1,16 @@
 module WelcomeHelper
   
+   def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+   
+  
 end
