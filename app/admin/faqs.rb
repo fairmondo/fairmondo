563ff7101b3ff17d47b9988feb83f5dcca2f6733
Michael Jurke <fairnmicha@gmx.de>
2012-11-08 00:39:43 +0100

    fixes faq index, refs #103

--- app/admin/faqs.rb
+++ app/admin/faqs.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Faq do
-  
+  # fix for #103
+  collection_action :index, :method => :get do
+    scope = Faq.scoped
+    scope.singleton_class.send(:include, Kaminari::ActiveRecordRelationMethods)
+    scope.singleton_class.send(:include, Kaminari::PageScopeMethods)
+        
+    @collection = scope.page(1) if params[:q].blank?
+    @search = scope.metasearch(clean_search_params(params[:q]))
+  end
 end
