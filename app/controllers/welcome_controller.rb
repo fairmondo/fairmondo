40e6c7faf89a34e468635440001e0b056c7cbbc1
Sebastian Sucker <sebastian@agile-workers.com>
2012-11-19 12:00:54 +0100

    Added auctions progress bar. Styling neeeded.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
-  
+
   before_filter :build_login
- 
   def index
- 
+    @auctions = Auction.count
+    @percentage = (@auctions.to_f/10000.0)*100.0
   end
-  
+
 end
