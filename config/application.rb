362dd71ac05e1d20d456c55d9bda49e888b467d2
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-11-29 20:00:36 +0100

    production server locale debug

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,9 @@ module Fairnopoly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
-    config.i18n.locale = :de
+    
+    #Workaround 
+    I18n.locale = config.i18n.locale = config.i18n.default_locale
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
