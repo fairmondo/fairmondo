641061a8b2d642a362beea9f01a9be674934c52f
Sebastian Sucker <sebastian@agile-workers.com>
2012-12-03 19:23:53 +0100

    Added tests for AuctionMailer.

--- spec/mailers/auction_mailer_spec.rb
+++ spec/mailers/auction_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe AuctionMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "report auction" do
+
+    before :each do
+      @auction = FactoryGirl.create(:auction)
+      @mail = AuctionMailer.report_auction(@auction)
+    end
+
+    it "renders the subject" do
+      @mail.subject.should have_content("Auction reported")
+    end
+
+    it "contains the auction id" do
+      @mail.subject.should eq ("Auction reported with ID: " + @auction.id.to_s)
+    end
+
+  end
+
 end
