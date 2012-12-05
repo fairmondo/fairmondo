a2693da7fe6350c6069da4e29b7b0d2a9aca7fc5
Sebastian Sucker <sebastian@agile-workers.com>
2012-12-05 12:18:14 +0100

    Added license texts & exluded tinycms from simplecov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 
 SimpleCov.start 'rails' do
   add_filter "app/mailers/notification.rb"
-  add_filter "app/helpers/application_helper.rb"
+  add_filter "gems/*"
 end
 
 ENV["RAILS_ENV"] ||= 'test'
