6b351c4a9b0157dcf1ced28cc121d42d755613a1
Sebastian Sucker <sebastian@agile-workers.com>
2012-12-06 23:56:44 +0100

    Fix for travis-ci.

--- test/functional/image_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ImageControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/userevents_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UsereventsHelperTest < ActionView::TestCase
-end
