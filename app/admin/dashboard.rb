cc584c3a27a763981f01f1c37c8e1107d3855eb5
andreasknoepfle <andreas.knoepfle@googlemail.com>
2012-12-14 17:42:23 +0100

    Log file last 100 lines viewer

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,10 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        h3 "Production Log"
-        span simple_format get_production_log
+    h3 "Production Log"
+    span simple_format get_production_log
         
-      end
-    end
+   
 
     # Here is an example of a simple dashboard with columns and panels.
     #
--- app/helpers/active_admin/view_helper.rb
+++ app/helpers/active_admin/view_helper.rb
@@ -1,10 +1,38 @@
-module ActiveAdmin::ViewHelper
-  def get_production_log
-    begin 
-      data = File.read(File.join(Rails.root,"log","production.log"))
-      data 
-    rescue
-      "Problem reading production log data!"
+class File
+
+    def tail(n)
+      buffer = 1024
+      idx = (size - buffer).abs
+      chunks = []
+      lines = 0
+  
+      begin
+        seek(idx)
+        chunk = read(buffer)
+        lines += chunk.count("\n")
+        chunks.unshift chunk
+        idx -= buffer
+      end while lines < ( n + 1 ) && pos != 0
+  
+      tail_of_file = chunks.join('')
+      ary = tail_of_file.split(/\n/)
+      lines_to_return = ary[ ary.size - n, ary.size - 1 ]
+  
     end
+end
+
+module ActiveAdmin::ViewHelper
+  
+
+  def get_production_log
+    begin
+      f = File.open(File.join(Rails.root,"log","production.log")) 
+      data = f.tail(100)
+      log = data.join("\n")
+    rescue
+      "Problem with the log file!"
+    end 
+   
   end
 end
+
