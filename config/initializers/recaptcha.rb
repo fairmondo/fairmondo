da589aa8f0d3da2cf7d97c00532b95e4c30547ef
Tobias Schmid <2toschmid@googlemail.com>
2012-12-17 12:16:26 +0100

    fix for travis ci

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,9 +1,13 @@
-
-api = YAML.load(File.read(File.expand_path(File.join( Rails.root, 'config', 'api.yml'))))
-
-Recaptcha.configure do |config|
+begin
+    
+  api = YAML.load(File.read(File.expand_path(File.join( Rails.root, 'config', 'api.yml'))))
   
-  config.public_key  = api['recaptcha']['public']
-  config.private_key = api['recaptcha']['private']
-  
+  Recaptcha.configure do |config|
+    
+    config.public_key  = api['recaptcha']['public']
+    config.private_key = api['recaptcha']['private']
+    
+  end
+rescue
+  puts 'api.yml not found'
 end
