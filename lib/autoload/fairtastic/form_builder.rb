ebf006461a5afa779ce1920e2feaad65b24531db
Michael Jurke <fairnmicha@gmx.de>
2012-12-20 23:11:40 +0100

    cleaning up plain_radio default for questionnaire, adding some locales

--- lib/autoload/fairtastic/form_builder.rb
+++ lib/autoload/fairtastic/form_builder.rb
@@ -7,6 +7,17 @@ module Fairtastic
   class FormBuilder < FormtasticBootstrap::FormBuilder
      
      include Fairtastic::Helpers::FieldsetWrapper
+=begin
+     def inputs(*args)
+       hint = args.last.is_a?(Hash) && args.last[:hint] 
+       if hint
+         l = hint_html
+       else
+         l = "".html_safe
+       end
+       l << super
+     end
+=end
      
      def input_with_explanation(*args)
        template.content_tag(:li,
@@ -20,13 +31,13 @@ module Fairtastic
      def explanation_radio_args(*args)
        options = args.extract_options!
        options[:prepend_label] = true
-       options[:as] = :plain_radio
+       options[:as] ||= :plain_radio
        args << options
      end
      
      def explanation_args(*args)
        options = args.extract_options!
-       options[:as] = :text
+       options[:as] = options[:explanation_as] || :text
        options[:label] = I18n.t('formtastic.labels.questionnaire.explanation')
        args[0] = (args[0].to_s << "_explanation").to_sym
        args << options
