dc0a215eec6f29d52829120b7b2faf18b7920c88
Michael Jurke <fairnmicha@gmx.de>
2013-01-10 23:05:38 +0100

    removes testing JavaScript

--- app/assets/javascripts/customize/toggle.js
+++ app/assets/javascripts/customize/toggle.js
@@ -1,16 +1,5 @@
 
-// refs #
-
-function xinspect(o,i){
-    if(typeof i=='undefined')i='';
-    if(i.length>50)return '[MAX ITERATIONS]';
-    var r=[];
-    for(var p in o){
-        var t=typeof o[p];
-        r.push(i+'"'+p+'" ('+t+') => '+(t=='object' ? 'object:'+xinspect(o[p],i+'  ') : o[p]+''));
-    }
-    return r.join(i+'\n');
-}
+// refs #151
 
 $(document).ready(function(){
 	$("input[data-select-toggle][type=checkbox]")
