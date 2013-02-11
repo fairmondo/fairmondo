21f0f26a70f6f0d2834254cdf63506cdd15af811
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-02-11 12:29:25 +0100

    bugfix for sebastian

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,8 @@ class Category < ActiveRecord::Base
   has_many :auctions_categories
   has_many :auctions, :through => :auctions_categories
   
-  validates :name, :uniqueness => true
+  # Doesn't work with our category tree
+  #validates :name, :uniqueness => true
   
   acts_as_nested_set
   
