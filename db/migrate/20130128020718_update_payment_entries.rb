80a653aa345a679686717ce7ea393cbcd7c119a2
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-02-11 13:38:26 +0100

    Validation Fix

--- db/migrate/20130128020718_update_payment_entries.rb
+++ db/migrate/20130128020718_update_payment_entries.rb
@@ -2,7 +2,7 @@ class UpdatePaymentEntries < ActiveRecord::Migration
   def up
     # without adoption, loading the record results in 
     # ActiveRecord::SerializationTypeMismatch: Attribute was supposed to be a Array, but was a String
-    Auction.unscoped.where("payment NOT LIKE '%[%]%'").each do |auction|
+    Auction.unscoped.all(:conditions => ["not(payment  LIKE '%[%]%')"]).each do |auction|
       auction.payment = "[" + auction.payment.to_s + "]"
       auction.save(validate: false)
     end
