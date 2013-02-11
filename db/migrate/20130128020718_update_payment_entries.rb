7d1074ffd39dc1b5047c94e7372fc9a89f9365b4
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-02-11 13:02:39 +0100

    Fixed migration to work with postgres

--- db/migrate/20130128020718_update_payment_entries.rb
+++ db/migrate/20130128020718_update_payment_entries.rb
@@ -2,7 +2,10 @@ class UpdatePaymentEntries < ActiveRecord::Migration
   def up
     # without adoption, loading the record results in 
     # ActiveRecord::SerializationTypeMismatch: Attribute was supposed to be a Array, but was a String
-    Auction.unscoped.update_all("payment = '[' + payment + ']'","payment NOT LIKE '%[%]%'")
+    Auction.unscoped.where("payment NOT LIKE '%[%]%'").each do |auction|
+      auction.payment = "[" + auction.payment + "]"
+      auction.save
+    end
   end
 
   def down
