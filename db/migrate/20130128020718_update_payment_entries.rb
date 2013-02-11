5fd45e545b066e6768270ca2dc6641a5f6536587
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-02-11 13:44:12 +0100

    Trying block form of unscoped

--- db/migrate/20130128020718_update_payment_entries.rb
+++ db/migrate/20130128020718_update_payment_entries.rb
@@ -2,10 +2,12 @@ class UpdatePaymentEntries < ActiveRecord::Migration
   def up
     # without adoption, loading the record results in 
     # ActiveRecord::SerializationTypeMismatch: Attribute was supposed to be a Array, but was a String
-    Auction.unscoped.all(:conditions => ["not(payment  LIKE ?)","[%]"]).each do |auction|
-      auction.payment = "[" + auction.payment.to_s + "]"
-      auction.save(validate: false)
-    end
+    Auction.unscoped {
+      Auction.all(:conditions => ["not(payment  LIKE ?)","[%]"]).each do |auction|
+        auction.payment = "[" + auction.payment.to_s + "]"
+        auction.save(validate: false)
+      end
+    }
   end
 
   def down
