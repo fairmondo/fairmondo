f1d91f178131f5eb92a575f45795f32f4a217ffe
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-02-25 18:04:36 +0100

    add forgotten file

---
+++ lib/autoload/fairtastic/helpers/input_helper.rb
@@ -0,0 +1,17 @@
+module Fairtastic
+  module Helpers
+    module InputHelper
+      
+      include FormtasticBootstrap::Helpers::InputHelper
+      
+      def input(method, options = {})
+
+        @input_step_with_errors ||= has_errors? method,options #set for input_step blocks
+
+        super
+        
+      end
+
+    end
+  end
+end
