775af22e2f05bd56fb57098f425ba7199b805e0a
Michael Jurke <fairnmicha@gmx.de>
2013-02-25 22:42:39 +0100

    removes debugger and unnecessary action overrides

--- app/controllers/auction_templates_controller.rb
+++ app/controllers/auction_templates_controller.rb
@@ -2,7 +2,7 @@ class AuctionTemplatesController < InheritedResources::Base
   
   before_filter :authenticate_user!
   before_filter :build_resource, :only => [:new, :create]
-  before_filter :build_auction, :only => [:new, :create]
+  before_filter :build_auction, :only => [:new, :edit, :create, :update]
   before_filter :setup_categories, :only => [:new, :edit, :create, :update]
   before_filter :build_questionnaires, :only => [:new, :edit, :create, :update]
   before_filter :build_transaction, :only => [:create]
@@ -12,19 +12,7 @@ class AuctionTemplatesController < InheritedResources::Base
   def begin_of_association_chain
     current_user
   end
-  
-  def create
-    create! do |success, failure|
-      success.html { redirect_to collection_url }
-    end
-  end
-  
-  def update
-    update! do |success, failure|
-      success.html { redirect_to collection_url }
-    end
-  end
-  
+    
   private 
   
   def build_auction
