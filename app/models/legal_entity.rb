c8bbf19df4202c1d0cecc94f201914925749d387
Tobias Schmid <2toschmid@googlemail.com>
2013-02-25 15:03:05 +0100

    user validations

--- app/models/legal_entity.rb
+++ app/models/legal_entity.rb
@@ -2,6 +2,10 @@ class LegalEntity < User
   
   attr_accessible :terms, :cancellation, :about
   
+  validates_presence_of :terms , :on => :update
+  validates_presence_of :cancellation , :on => :update
+  validates_presence_of :about , :on => :update
+  
   def legal_entity_terms_ok
       if( self.terms && !self.terms.empty? &&
           self.cancellation && !self.cancellation.empty? &&
