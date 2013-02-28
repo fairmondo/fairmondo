daa585c034488c9997e69cfd33ca1bad3f42d77c
Tobias Schmid <2toschmid@googlemail.com>
2013-02-28 14:30:09 +0100

    save password fix

--- app/models/legal_entity.rb
+++ app/models/legal_entity.rb
@@ -6,6 +6,11 @@ class LegalEntity < User
   validates_presence_of :cancellation , :on => :update
   validates_presence_of :about , :on => :update
   
+    # see http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
+    def self.model_name
+      User.model_name
+    end
+  
   def legal_entity_terms_ok
       if( self.terms && !self.terms.empty? &&
           self.cancellation && !self.cancellation.empty? &&
