7342f5d644d24f35bbe60662a1bc4c5eb92f70d7
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-03-06 18:23:05 +0100

    removing security disables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,7 @@ Fairnopoly::Application.configure do
   Paperclip.options[:command_path] = "/usr/bin"
 
   ActionMailer::Base.smtp_settings  = YAML.load(File.read(File.expand_path(File.join( Rails.root, 'config', 'actionmailer.yml'))))
-  ActionMailer::Base.smtp_settings[:openssl_verify_mode] = false
-  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+ 
   
   
 end
