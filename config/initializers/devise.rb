f83829ef458c483aa17c72d539dc28341c4759d0
Tobias Schmid <2toschmid@googlemail.com>
2013-03-13 15:37:45 +0100

    set reconfirmable true

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -100,7 +100,7 @@ Devise.setup do |config|
   # db field (see migrations). Until confirmed new email is stored in
   # unconfirmed email column, and copied to email column on successful confirmation.
   #config.reconfirmable = true
-  config.reconfirmable = false
+  config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
