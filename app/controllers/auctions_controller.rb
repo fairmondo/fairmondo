f111c120948ea427511327c73d15a4391d9adb04
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-03-19 16:45:53 +0100

    Preview done

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -56,11 +56,25 @@ class AuctionsController < ApplicationController
   # GET /auctions/1.json
   def show
     @search_cache = Auction.new(params[:auction])
-    @auction = Auction.find(params[:id])
+    @auction = Auction.unscoped.find(params[:id])
 
-    @libraries = @auction.libraries.public.paginate(:page => params[:page], :per_page=>10)
-    #@seller_products = @auction.seller.auctions.where('id != ?',@auction.id).paginate(:page => params[:page], :per_page=>18)
-    @seller_products = @auction.seller.auctions.paginate(:page => params[:page], :per_page=>18)
+    if @auction.active 
+      @libraries = @auction.libraries.public.paginate(:page => params[:page], :per_page=>10)
+      #@seller_products = @auction.seller.auctions.where('id != ?',@auction.id).paginate(:page => params[:page], :per_page=>18)
+      @seller_products = @auction.seller.auctions.paginate(:page => params[:page], :per_page=>18)
+    else 
+      if current_user && current_user.id == @auction.seller.id
+        @auction.calculate_fees_and_donations
+      else
+         flash[:error] = t('auction.notices.inactive')
+         begin
+            redirect_to :back
+         rescue ActionController::RedirectBackError 
+            redirect_to auctions_path
+         end
+         return
+      end
+    end
 
     set_title_image_and_thumbnails
     
@@ -88,7 +102,7 @@ class AuctionsController < ApplicationController
       # end
       if !legal_entity.valid?
          #flash[:error] = private_user.errors
-         flash[:error] = t('auction.show.error')
+         flash[:error] = t('auction.notices.incomplete_profile')
          redirect_to url_for :controller => "dashboard", :action => "edit_profile"
          return
        end
@@ -96,7 +110,7 @@ class AuctionsController < ApplicationController
        private_user = current_user.becomes(PrivateUser)
        if !private_user.valid?
          #flash[:error] = private_user.errors
-         flash[:error] = t('auction.show.error')
+         flash[:error] = t('auction.notices.incomplete_profile')
          redirect_to url_for :controller => "dashboard", :action => "edit_profile"
          return
        end
@@ -133,7 +147,14 @@ class AuctionsController < ApplicationController
 
   # GET /auctions/1/edit
   def edit
-    @auction = Auction.with_user_id(current_user.id).find(params[:id])
+    @auction = Auction.unscoped.with_user_id(current_user.id).find(params[:id])
+    if @auction.locked
+      #Todo: Give oportunity to create new article based on this article
+      flash[:error] = t('auction.notices.locked')
+      redirect_to @auction
+      return
+    end
+    
     setup_form_requirements
     respond_to do |format|
       format.html 
@@ -173,7 +194,7 @@ class AuctionsController < ApplicationController
   # PUT /auctions/1.json
   def update
     
-    @auction = Auction.with_user_id(current_user.id).find(params[:id])
+    @auction = Auction.unscoped.with_user_id(current_user.id).find(params[:id])
 
   
       if @auction.update_attributes(params[:auction]) && build_and_save_template(@auction)
@@ -196,31 +217,34 @@ class AuctionsController < ApplicationController
     
   end
 
-  def preview
-    @auction = Auction.find(params[:id])
-    if @auction.active || (current_user != @auction.seller) # false preview
-       redirect_to :show
-    else
-      @auction.calculate_fees_and_donations
-      set_title_image_and_thumbnails
-      respond_to do |format|
-        format.html # show.html.erb
-        format.json { render :json => @auction }
-      end
-    end
-  end
-
   def activate
-    if @auction.active || (current_user != @auction.seller) # false activate
-       redirect_to :show
+    @auction = Auction.unscoped.with_user_id(current_user.id).find(params[:id])
+    if @auction.active || (current_user.id != @auction.seller.id) # false activate
+       redirect_to @auction
     else
-      @auction = Auction.find(params[:id])
       @auction.calculate_fees_and_donations
       @auction.locked = true # Lock The Auction
       @auction.active = true # Activate to be searchable
       @auction.save
       respond_to do |format|
         format.html { redirect_to @auction, :notice => I18n.t('auction.notices.create') }
+        format.json { render :json => @auction, :status => :created, :location => @auction }
+      end
+    end
+  end
+  
+  def deactivate
+    @auction = Auction.unscoped.with_user_id(current_user.id).find(params[:id])
+    
+    #REMEMBER: AuctionTransaction may not be removed if running
+    
+    if current_user.id != @auction.seller.id # false activate
+       redirect_to @auction
+    else
+      @auction.active = false 
+      @auction.save
+      respond_to do |format|
+        format.html { redirect_to @auction, :notice => I18n.t('auction.notices.deactivated') }
         format.json { render :json => @auction, :status => :created, :location => @auction }
       end
     end
@@ -286,7 +310,7 @@ class AuctionsController < ApplicationController
     #Throwing User Events
     Userevent.new(:user => current_user, :event_type => UsereventType::AUCTION_CREATE, :appended_object => @auction).save
     respond_to do |format|
-      format.html { redirect_to preview_auction_path(@auction) }
+      format.html { redirect_to auction_path(@auction) }
       format.json { render :json => @auction, :status => :created, :location => @auction }
     end
   end
