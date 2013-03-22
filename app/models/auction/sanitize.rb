d308022ba4992c5e5f126d9873671fbd3e8fc611
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-03-22 13:03:05 +0100

    added missing files

---
+++ app/models/auction/sanitize.rb
@@ -0,0 +1,16 @@
+module Auction::Sanitize
+  extend ActiveSupport::Concern
+  
+   # lib dependency
+  include SanitizeTinyMce
+  
+  
+  included do
+    before_validation :sanitize_content, :on => :create 
+    
+  end
+  def sanitize_content
+    self.content = sanitize_tiny_mce(self.content)
+  end
+  
+end
---
+++ lib/autoload/sanitize_tiny_mce.rb
@@ -0,0 +1,7 @@
+module SanitizeTinyMce
+  def sanitize_tiny_mce(field)
+    ActionController::Base.helpers.sanitize(field,
+    :tags => %w(a b i strong em p param h1 h2 h3 h4 h5 h6 br hr ul li img),
+    :attributes => %w(href name src type value width height data style) );
+  end
+end
