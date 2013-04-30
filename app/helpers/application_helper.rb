1e9726367886594661c315ccf1f85ad00bf0e9ed
Tobias Schmid <2toschmid@googlemail.com>
2013-04-30 16:43:19 +0200

    meta tags for SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -97,4 +97,30 @@ module ApplicationHelper
     tip += "</span></a>"
     tip.html_safe
   end
+  
+  def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? content_for(:title) + t('auction.show.title_addition') : t('common.fairnopoly')
+    end
+  end
+  
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), t('meta_tags.keywords')].join(', ') : t('meta_tags.keywords')
+    end
+  end
+
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : t('meta_tags.description')
+    end
+  end
+  
+  
 end
