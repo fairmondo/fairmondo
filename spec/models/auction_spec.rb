2afe63a960399061fc3668ab8465a428b1c89eee
Konstantin K <konstantin.koss@gmail.com>
2013-05-06 14:13:43 +0200

    add more auction specs

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -2,22 +2,71 @@ require 'spec_helper'
 
 describe Auction do
 
-  let(:auction) { FactoryGirl::create(:auction)}
+  let(:auction) { FactoryGirl::create(:auction) }
+  subject { auction }
 
   it {should have_many :images}
 
   it {should belong_to :seller}
   it {should have_many :categories}
 
-  it "returns the title image" do
-    auction = FactoryGirl.create(:auction)
+  it "should return the title image" do
     image = FactoryGirl.create(:image, :auction => auction)
     auction.title_image
   end
 
-  describe "Auction::FeesAndDonations" do
-    it "should return the friendly_percent_calculated" do
-      auction.friendly_percent_calculated.should
+  describe "with_user_id scope" do
+    before do
+      @auction_with_id = FactoryGirl.create(:auction)
+      @auction_without_id = FactoryGirl.create(:auction).user_id = nil
+    end
+
+    it "should return all auctions with a user_id when called with 'true'" do
+      results = Auction.with_user_id(true)
+      results.should include(@auction_with_id)
+      results.should_not include(@auction_without_id)
+    end
+
+    it "should return all auctions with a specific user_id when called with an integer" do
+      auction_with_correct_id = FactoryGirl.create(:auction)
+      results = Auction.with_user_id(auction_with_correct_id.user_id)
+      results.should include(auction_with_correct_id)
+      results.should_not include(@auction_with_id)
+      results.should_not include(@auction_without_id)
+    end
+  end
+
+  context "Auction::Initial" do
+    it "should rescue MissingAttributeErrors" do
+      auction.stub(:new_record?) { raise ActiveModel::MissingAttributeError }
+      auction.initialize_values.should_not raise_error(ActiveModel::MissingAttributeError)
+    end
+  end
+
+  context "Auction::FeesAndDonations" do
+    context "friendly_percent_calculated" do
+      it "should call friendly_percent_result" do
+        auction.should_receive :friendly_percent_result
+        auction.friendly_percent_calculated
+      end
+    end
+    context "fee_percentage" do
+      it "should return the fair percentage when auction.fair" do
+        auction.fair = true
+        auction.send('fee_percentage').should == 0.03
+      end
+
+      it "should return the default percentage when !auction.fair" do
+        auction.send('fee_percentage').should == 0.06
+      end
+    end
+  end
+
+  context "Auction::Attributes" do
+    it "should throw an error if default_transport_selected isn't able to call the transport function" do
+      auction.default_transport.should be_true
+      auction.stub(:send).and_return(false)
+      expect { auction.default_transport_selected }.to raise_error
     end
   end
 end
