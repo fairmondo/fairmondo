dd84c8b40403c69d5dccfd026a989272e3e123d1
Konstantin K <konstantin.koss@gmail.com>
2013-05-07 14:27:33 +0200

    refactor LegalEntity.legal_entity_terms_ok

--- app/models/legal_entity.rb
+++ app/models/legal_entity.rb
@@ -20,26 +20,22 @@ class LegalEntity < User
   validates_presence_of :cancellation , :on => :update
   validates_presence_of :about , :on => :update
 
-    # see http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
-    def self.model_name
-      User.model_name
-    end
-
-  def legal_entity_terms_ok
-      if( self.terms && !self.terms.empty? &&
-          self.cancellation && !self.cancellation.empty? &&
-          self.about && !self.about.empty?)
-        return true
-      else
-        return false
-      end
+  # see http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
+  def self.model_name
+    User.model_name
   end
 
-   def sanitize
-      self.about = sanitize_tiny_mce(self.about)
-      self.cancellation = sanitize_tiny_mce(self.cancellation)
-      self.terms = sanitize_tiny_mce(self.terms)
-      self.about_me = sanitize_tiny_mce(self.about_me)
-    end
+  def legal_entity_terms_ok
+    ( self.terms && !self.terms.empty? &&
+      self.cancellation && !self.cancellation.empty? &&
+      self.about && !self.about.empty? )
+  end
+
+  def sanitize
+    self.about = sanitize_tiny_mce(self.about)
+    self.cancellation = sanitize_tiny_mce(self.cancellation)
+    self.terms = sanitize_tiny_mce(self.terms)
+    self.about_me = sanitize_tiny_mce(self.about_me)
+  end
 
 end
