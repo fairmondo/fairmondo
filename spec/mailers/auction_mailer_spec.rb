a3a7d0f3bd6d4bb9ed34dd9dc046432e073b0427
Konstantin K <konstantin.koss@gmail.com>
2013-05-07 14:27:59 +0200

    even more model specs

--- spec/mailers/auction_mailer_spec.rb
+++ spec/mailers/auction_mailer_spec.rb
@@ -1,22 +1,24 @@
 require "spec_helper"
 
 describe AuctionMailer do
-
-  describe "report auction" do
-
-    before :each do
-      @auction = FactoryGirl.create(:auction)
-      @mail = AuctionMailer.report_auction(@auction,"text")
-    end
+  describe "report_auction" do
+    let(:auction) { FactoryGirl.create(:auction) }
+    let(:mail) { AuctionMailer.report_auction(auction,"text") }
 
     it "renders the subject" do
-      @mail.subject.should have_content("Auction reported")
+      mail.subject.should have_content("Auction reported")
     end
 
     it "contains the auction id" do
-      @mail.subject.should eq ("Auction reported with ID: " + @auction.id.to_s)
+      mail.subject.should eq("Auction reported with ID: " + auction.id.to_s)
     end
-
   end
 
+  describe "category_proposal" do
+    it "should call the mail function" do
+      a = AuctionMailer.send("new")
+      a.should_receive(:mail).with(to: 'kundenservice@fairnopoly.de', subject: "Category proposal: foobar" ).and_return true
+      a.category_proposal("foobar")
+    end
+  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -6,5 +6,4 @@ describe Image do
   end
 
   it {should belong_to :auction}
-  
 end
