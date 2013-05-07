b9faae019bd86be4db115767a3579827a78fa6cc
Tobias Schmid <2toschmid@googlemail.com>
2013-05-07 16:51:40 +0200

    basic price for legal users

---
+++ db/migrate/20130507132242_basic_price_attributes.rb
@@ -0,0 +1,9 @@
+class BasicPriceAttributes < ActiveRecord::Migration
+ 
+  def change
+    add_money :auctions, :basic_price, currency: { present: false }
+    add_column :auctions, :basic_price_amount, :string
+
+  end
+  
+end
