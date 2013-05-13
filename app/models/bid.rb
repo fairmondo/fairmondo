96c52d14f497ec44e3de2eaa9c5bc69c6161acce
Konstantin K <konstantin.koss@gmail.com>
2013-05-13 17:16:52 +0200

    comment out dead code

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -10,17 +10,18 @@ class Bid < ActiveRecord::Base
   # TODO
   # before_save :check_better
   validates_presence_of :user_id, :auction_transaction_id, :price_cents
-  def check_better
-    if self.auction.transaction.max_bid
-      unless self.price_cents > self.auction_transaction.max_bid
-        errors[:price_cents] << I18n.t("transaction.bid.smaller-than-prev")
-      end
-    else
-      unless self.price_cents >= self.auction_transaction.auction.price_cents
-        errors[:price_cents] <<  I18n.t("transaction.bid.smaller-than-init")
-      end
-    end
 
-  end
+  ### Comment back in when this is used and testable
+  # def check_better
+  #   if self.auction.transaction.max_bid
+  #     unless self.price_cents > self.auction_transaction.max_bid
+  #       errors[:price_cents] << I18n.t("transaction.bid.smaller-than-prev")
+  #     end
+  #   else
+  #     unless self.price_cents >= self.auction_transaction.auction.price_cents
+  #       errors[:price_cents] <<  I18n.t("transaction.bid.smaller-than-init")
+  #     end
+  #   end
+  # end
 
 end
