8335ac4ab8300b6e223be155cd842650c86424fa
Konstantin K <konstantin.koss@gmail.com>
2013-05-13 12:44:12 +0200

    formatting and DRYing

--- lib/autoload/fairtastic/form_builder.rb
+++ lib/autoload/fairtastic/form_builder.rb
@@ -7,52 +7,49 @@ module Fairtastic
     #include Fairtastic::Helpers::FieldsetWrapper
     include Fairtastic::Helpers::InputHelper
     include Fairtastic::Inputs::Base::InputSteps
-    
+
     def inputs(*args, &block)
       super(*extended_fieldset_args(*args),&block)
     end
-    
-    def input_with_purpose(*args)
-       template.content_tag(:li,
-      template.content_tag(:fieldset, 
-      template.content_tag(:ol,
-      input(*extended_radio_args(*args)) << input(*pupose_args(*args))),:class => "inputs"),
-      :class => "questionnaire-entry"
-      )
-      
-      
-    end
-    
-    def semantic_errors(*args)   
-      args.inject([]) do |array, method|
-          errors = Array(@object.errors[method.to_sym]).to_sentence
-          @input_step_with_errors ||=errors.present?
+
+    # DRYing input_with_purpose and input_with_explanation
+    def input_with (specification, *args)
+      input_args = case specification
+        when :purpose then purpose_args(*args)
+        when :explanation then explanation_args(*args)
+        else raise ArgumentError, "input_with called with an unfamiliar argument"
       end
-      super
-      
+
+      template.content_tag(
+        :li,
+        template.content_tag(
+          :fieldset,
+          template.content_tag(
+            :ol,
+            input(*extended_radio_args(*args)) << input(*input_args)
+          ),
+          :class => "inputs"
+        ),
+        :class => "questionnaire-entry"
+      )
     end
 
-    def input_with_explanation(*args)
-     
-        
-    
-      template.content_tag(:li,
-      template.content_tag(:fieldset, 
-      template.content_tag(:ol,
-       input(*extended_radio_args(*args)) <<
-         input(*explanation_args(*args))),:class => "inputs"),
-      :class => "questionnaire-entry"
-      )
+    def semantic_errors(*args)
+      args.inject([]) do |array, method|
+        errors = Array(@object.errors[method.to_sym]).to_sentence
+        @input_step_with_errors ||=errors.present?
+      end
+      super
     end
-         
+
     def nested_inputs_for(association_name, options = {}, &block)
       title = options[:name]
       title = localized_string(association_name, object, nil) if title == true
       title ||= ""
-      
+
       tooltip = optional_tooltip_html(association_name, options)
       tooltip = template.content_tag(:span, tooltip) if tooltip.present?
-      
+
       title = template.content_tag(:h5, (title << tooltip).html_safe, :class => "questionnaire-entry")
       title.html_safe << semantic_fields_for(association_name, options, &block)
     end
@@ -65,10 +62,10 @@ module Fairtastic
       options[:label] = true
       args << options
     end
-    
+
     def extended_fieldset_args(*args)
       options = args.extract_options!
-      if options[:class] 
+      if options[:class]
         options[:class] << " inputs"
       else
          options[:class] = "inputs"
@@ -76,7 +73,7 @@ module Fairtastic
       args << options
     end
 
-    def pupose_args(*args)
+    def purpose_args(*args)
       options = args.extract_options!
       options[:as] = options[:purpose_as] || :plain_check_boxes
       args[0] = (args[0].to_s << "_purposes").to_sym
@@ -92,5 +89,4 @@ module Fairtastic
     end
 
   end
-
 end
--- lib/autoload/fairtastic/inputs/base/input_steps.rb
+++ lib/autoload/fairtastic/inputs/base/input_steps.rb
@@ -1,36 +1,34 @@
-
-
 module Fairtastic
   module Inputs
     module Base
       module InputSteps
-        
+
         def input_step(step_key, options = {}, &block)
           css = "box"
           css << " default-step" if options[:default_step]
-          
+
           if options[:class]
             options[:class] << " #{step_key}-step-inputs"
           else
             options[:class] = "#{step_key}-step-inputs"
           end
-          
-          
+
+
           #evaluate the block before setting css class for errors
           block_content = inputs(options.except(:tooltip), &block)
-      
+
           #if we detect an error at an input we set the error class of input step
           #after setting reset the block error
           if @input_step_with_errors
             css << " error-box"
             @input_step_with_errors = false
           end
-          
+
           template.content_tag(:div,
           step_heading_html(step_key, options) << template.content_tag(:div,block_content, :class => "white-well box-content" ),
           :class => css, :id => "#{step_key}_step"
           )
-          
+
         end
 
         private
@@ -58,16 +56,16 @@ module Fairtastic
           )  << template.content_tag(:div,"",:class=>"clearfix"), :class => "box-legend"
           )
         end
-        
+
         def optional_tooltip_html(method, options = {})
           tooltip_text = options[:tooltip]
           tooltip_text = localized_string(method, nil, "tooltips") if options[:tooltip] == true
           if tooltip_text
-            template.content_tag(:a, "",:class => "input-tooltip", "data-content" => tooltip_text) 
+            template.content_tag(:a, "",:class => "input-tooltip", "data-content" => tooltip_text)
           else
             ""
           end
-        end        
+        end
 
       end
     end
