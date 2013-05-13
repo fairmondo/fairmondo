b8051fbd86f5b7b50237ff322e16f81193d30590
Konstantin K <konstantin.koss@gmail.com>
2013-05-13 12:44:12 +0200

    formatting and DRYing

--- lib/autoload/fairtastic/helpers/input_helper.rb
+++ lib/autoload/fairtastic/helpers/input_helper.rb
@@ -1,15 +1,15 @@
 module Fairtastic
   module Helpers
     module InputHelper
-      
+
       include Formtastic::Helpers::InputHelper
-      
+
       def input(method, options = {})
 
         @input_step_with_errors ||= has_errors? method,options #set for input_step blocks
 
         super
-        
+
       end
 
     end
