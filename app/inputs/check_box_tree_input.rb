07d7f6cee10ee3f78c953b8779404527047542bd
Konstantin K <konstantin.koss@gmail.com>
2013-05-16 11:33:03 +0200

    formatting

--- app/inputs/check_box_tree_input.rb
+++ app/inputs/check_box_tree_input.rb
@@ -8,7 +8,7 @@ class CheckBoxTreeInput < Formtastic::Inputs::CheckBoxesInput
         hidden_field_for_all <<
          template.content_tag(:div,
           recursive_to_html(collection, true) ,:class => "check_box_tree_contents")
-          
+
     end
   end
 
@@ -27,18 +27,22 @@ class CheckBoxTreeInput < Formtastic::Inputs::CheckBoxesInput
   # the li-element is for the tree
   # the span element wrapps the content (e.g. for borders)
   def tree_item_wrapping(tree_class,&block)
-    template.content_tag(:li,
-    template.content_tag(:span,
-    template.capture(&block)
-    ), :class => tree_class).html_safe
+    template.content_tag(
+      :li,
+      template.content_tag(
+        :span,
+        template.capture(&block)
+      ),
+      :class => tree_class
+    ).html_safe
   end
-  
-  
+
+
 
   def choice_html(choice)
     (hidden_fields? ? check_box_with_hidden_input(choice) : check_box_without_hidden_input(choice)).html_safe <<
     template.content_tag(:label,  choice_label(choice), label_html_options.merge(:for => choice_input_dom_id(choice)))
-   
+
   end
 
   private
@@ -58,7 +62,7 @@ class CheckBoxTreeInput < Formtastic::Inputs::CheckBoxesInput
     else
       h <<   collection.map { |choice|
                    recursive = recursive_to_html(choice.last)
-                   
+
                       choices_wrapping do
                          choices_group_wrapping do
                             tree_item_wrapping(( recursive.empty? ? "leaf-item" : "tree-item" ).html_safe) do
@@ -67,9 +71,9 @@ class CheckBoxTreeInput < Formtastic::Inputs::CheckBoxesInput
                             end
                           end
                       end
-                   
+
                 }.join("\n").html_safe
-   
+
     end
 
   end
