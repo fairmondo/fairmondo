a25e7c7f98aff342ce5948c283c121e2dd9ad677
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-05-24 14:41:18 +0200

    mailer multipart best practice

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -6,15 +6,15 @@ class Notification < ActionMailer::Base
   #
   #   en.notification.invitation.subject
   #
-  def invitation(id, user, name, email, activation_key)
-
-    key = SecureRandom.hex(24)
-    @user = user
-    @name = name
-    @url  = "http://beta.fairnopoly.de/confirm_invitation?id=" + id.to_s + "&key=" + activation_key
-
-    mail(:to => email, :subject => ("Einladung auf Fairnopoly von "+@user.name) )
-
-  end
+  #def invitation(id, user, name, email, activation_key)
+  #
+  #  key = SecureRandom.hex(24)
+  #  @user = user
+  #  @name = name
+  #  @url  = "http://beta.fairnopoly.de/confirm_invitation?id=" + id.to_s + "&key=" + activation_key
+  #
+  #  mail(:to => email, :subject => ("Einladung auf Fairnopoly von "+@user.name) )
+  #
+  #end
 
 end
