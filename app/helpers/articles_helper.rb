a078a6a4c8d090c4c83cff8c497ab2d508b50994
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-05-24 14:14:21 +0200

    resolved coverage issue

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -24,17 +24,6 @@ module ArticlesHelper
     bclass
   end
 
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 
   def get_category_tree category
     tree = []
