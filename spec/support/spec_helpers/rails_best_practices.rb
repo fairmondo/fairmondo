51071b1e3165512f7619888d66f4a79eb8509f33
Konstantin K <konstantin.koss@gmail.com>
2013-05-25 09:14:11 +0200

    fix last merge

--- spec/support/spec_helpers/rails_best_practices.rb
+++ spec/support/spec_helpers/rails_best_practices.rb
@@ -14,6 +14,7 @@ RSpec.configure do |config|
       bp_analyzer.instance_variable_set :@options, options.merge({'format' => 'html'})
       bp_analyzer.output
 
-    exit bp_analyzer.runner.errors.size
+      exit 1 if bp_analyzer.runner.errors.size > 0
+    end
   end
 end
