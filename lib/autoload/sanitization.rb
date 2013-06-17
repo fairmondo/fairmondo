7a97c7d1a2ec005431f426669eace2f8cbc01b43
Konstantin K <konstantin.koss@gmail.com>
2013-06-17 12:11:08 +0200

    refactor sanitization module

--- lib/autoload/sanitization.rb
+++ lib/autoload/sanitization.rb
@@ -2,9 +2,6 @@
 module Sanitization
   extend ActiveSupport::Concern
 
-  # lib dependency
-  include SanitizeTinyMce
-
   protected
   # DSL method to sanitize specific fields automatically before the validation
   # step.
@@ -16,12 +13,12 @@ module Sanitization
   #   + other before_validate params
   # @return [undefined]
   def auto_sanitize *fields
-    options = {} unless (options = fields.last).class == Hash
+    options = {} unless (options = fields.last).is_a? Hash
     options.reverse_merge! method: 'clean'
 
     # For each field: define a new method, then register a callback to that method
     fields.each do |field|
-      if field.class == Symbol
+      if field.is_a? Symbol
         method_name = "sanitize_#{options[:method]}_#{field}"
         define_method method_name, send("#{options[:method]}_sanitization", field)
         before_validation method_name.to_sym, options
@@ -39,7 +36,28 @@ module Sanitization
   # Method content for sanitize_tiny_mce_X callbacks
   # @api private
   def tiny_mce_sanitization field
-    Proc.new { self.send("#{field}=", SanitizeTinyMce::sanitize_tiny_mce(self.send(field))) }
+    Proc.new { self.send("#{field}=", Sanitization.sanitize_tiny_mce(self.send(field))) }
+  end
+
+  # Sanitization specifically for tiny mce fields which allow certain HTML
+  # elements.
+  #
+  # @api private
+  # @param field [Sting] The content to sanitize
+  # @return [String] The sanitized content
+  def self.sanitize_tiny_mce field
+    Sanitize.clean(field,
+      elements: %w(a b i strong em p h1 h2 h3 h4 h5 h6 br hr ul li img),
+      attributes: {
+        'a' => ['href', 'type'],
+        'img' => ['src'],
+        :all => ['width', 'height', 'style', 'data', 'name']
+      },
+      protocols: {
+        'a' => { 'href' => ['ftp', 'http', 'https', 'mailto'] },
+        'img' => { 'src' => ['http', 'https'] }
+      }
+    )
   end
 
 end
