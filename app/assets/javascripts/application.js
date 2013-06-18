42065c54879f2f3abda03060b58240382027c115
Andreas Knöpfle <andi@fairnopoly.de>
2013-06-18 09:21:59 +0200

    js categories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,13 +27,11 @@
 //= require tinymce
 //= require tinymce-jquery
 //= require bootstrap
-//= require jquery.complexify
-//= require jquery.hashchange
-//= require jquery.socialshareprivacy
+
 
 //= require_tree ./inputs
 //= require_tree ./models
 //= require_tree ./customize
 //= require_tree ./visual
-
+//= require_tree ./vendor
 //= require_tree ./packery
