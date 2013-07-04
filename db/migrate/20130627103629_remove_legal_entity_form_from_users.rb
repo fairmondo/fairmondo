ba0f56c38fa51fdc7753c7a625ae2b3523627afc
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-07-04 14:26:32 +0200

    company name confusion

--- db/migrate/20130627103629_remove_legal_entity_form_from_users.rb
+++
@@ -1,7 +0,0 @@
-class RemoveLegalEntityFormFromUsers < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20130627104158_remove_company_name_from_user.rb
+++
@@ -1,7 +0,0 @@
-class RemoveCompanyNameFromUser < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
