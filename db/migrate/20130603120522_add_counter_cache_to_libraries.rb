cfea7201bdb7e44ec1e06800a8a7f5fbb8c7b6a8
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-07-09 13:58:39 +0200

    Compressed and cleaned up migrations

--- db/migrate/20130603120522_add_counter_cache_to_libraries.rb
+++
@@ -1,5 +0,0 @@
-class AddCounterCacheToLibraries < ActiveRecord::Migration
-  def change
-    add_column :libraries, :library_elements_count, :integer, default: 0
-  end
-end
--- db/migrate/20130611115910_remove_active_from_article.rb
+++
@@ -1,9 +0,0 @@
-class RemoveActiveFromArticle < ActiveRecord::Migration
-  def up
-    remove_column :articles, :active
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
