3b743e03d740d2b1de1c7df9ffee4b9d102edcf5
Konstantin K <konstantin.koss@gmail.com>
2013-07-09 12:50:51 +0200

    bugfix for #97: Dummy pioneer is shown when no article was found to show

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,2 +1,8 @@
 # For rails-settings-cached
-Settings.defaults[:featured_article_id] = 1
+begin
+  last_article = Article.last
+rescue
+  last_article = nil
+ensure
+  Settings.defaults[:featured_article_id] = last_article ? last_article.id : nil
+end
