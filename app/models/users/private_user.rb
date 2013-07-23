d08eed078987e70af3d83efc5ce03c058f422ec9
nina <nina.gaensdorfer@gmx.de>
2013-07-23 14:11:06 +0200

    Changed some comments

--- app/models/users/private_user.rb
+++ app/models/users/private_user.rb
@@ -45,7 +45,7 @@ class PrivateUser < User
 
 
   state_machine :seller_state, :initial => :standard do
-    # if more than 90% positive ratings in the last 50 ratings:
+
     event :rate_up_to_good do
       transition :standard => :good
     end
