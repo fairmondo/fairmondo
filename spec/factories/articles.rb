fde6a403c3130ca62f172ab97879052875a5e891
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-07-29 14:26:40 +0200

    reworked fair alternative

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -95,5 +95,13 @@ FactoryGirl.define do
         article.images = [FactoryGirl.build(:fixture_image)]
       end
     end
+
+    trait :fair do
+      fair true
+      fair_kind :fair_seal
+      fair_seal :trans_fair
+    end
+
+
   end
 end
