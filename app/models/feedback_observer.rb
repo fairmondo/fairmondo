01c173f864f8cc611793d37adb3227571bd611a4
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-07-31 16:24:35 +0200

    Fix #186 + adding subject prefix for help as well

--- app/models/feedback_observer.rb
+++ app/models/feedback_observer.rb
@@ -28,8 +28,10 @@ class FeedbackObserver < ActiveRecord::Observer
       when "report_article" then
         ArticleMailer.report_article( feedback.article,feedback.user,feedback.text ).deliver
       when "send_feedback" then
+        feedback.subject.prepend("[Feedback] ")
         FeedbackMailer.feedback_and_help( feedback, feedback.feedback_subject ).deliver
       when "get_help" then
+        feedback.subject.prepend("[Hilfe] ")
         FeedbackMailer.feedback_and_help( feedback, feedback.help_subject ).deliver
     end
 
