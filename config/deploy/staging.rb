f502467a86104cedffe1a4f3c8bad07a6eeb9768
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-08-06 18:41:49 +0200

    threw out debugger

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -18,8 +18,6 @@
 # along with Farinopoly.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require 'debugger'
-
 role :app, "78.109.61.137", :primary => true
 role :web, "78.109.61.137", :primary => true
 role :db, "78.109.61.137", :primary => true
