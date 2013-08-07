2007fab645ab4174fd7aab6d79a225de4307c6e8
Konstantin <konstantin@fairnopoly.de>
2013-08-07 15:23:45 +0200

    refactor and speed up test suite

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 #Rails
 gem 'rails', '~> 3.2.13'
-
+gem 'perftools.rb'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
@@ -133,6 +133,8 @@ group :development, :test do
   # test suite additions
   gem "rails_best_practices"
   gem "brakeman" # security test: execute with 'brakeman'
+  gem 'parallel_tests'
+  gem 'rspec-instafail'
 
   # Replace Webrick
   gem 'thin'
@@ -149,9 +151,6 @@ group :development do
   # HAML Conversion tools
   gem "erb2haml"
   gem "html2haml"
-
-  # Clean code before commiting
-  gem "code-cleaner"
 
   #zipruby for icecat catalog download
   gem "zipruby"
--- spec/support/spec_helpers/rails_best_practices.rb
+++ spec/support/spec_helpers/rails_best_practices.rb
@@ -20,22 +20,20 @@
 # along with Fairnopoly.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Check for best practices
-RSpec.configure do |config|
-  config.after :suite do
-    unless $skip_audits
-      puts "\n\n[RailsBestPractices] Testing:\n".underline
-      bp_analyzer = RailsBestPractices::Analyzer.new(Rails.root, {})
-      bp_analyzer.analyze
+after_suite do
+  unless $skip_audits
+    puts "\n\n[RailsBestPractices] Testing:\n".underline
+    bp_analyzer = RailsBestPractices::Analyzer.new(Rails.root, {})
+    bp_analyzer.analyze
 
-      # Console output:
-      bp_analyzer.output
+    # Console output:
+    bp_analyzer.output
 
-      # Generate HTML as well:
-      options = bp_analyzer.instance_variable_get :@options
-      bp_analyzer.instance_variable_set :@options, options.merge({'format' => 'html'})
-      bp_analyzer.output
+    # Generate HTML as well:
+    options = bp_analyzer.instance_variable_get :@options
+    bp_analyzer.instance_variable_set :@options, options.merge({'format' => 'html'})
+    bp_analyzer.output
 
-      exit 1 if bp_analyzer.runner.errors.size > 0
-    end
+    exit 1 if bp_analyzer.runner.errors.size > 0
   end
 end
