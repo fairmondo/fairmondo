2ba14d1269e8e28a7cec9ce63a2f6fa0815873c8
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-08-14 15:34:23 +0200

    Improved Eager loading and Transaction generation

--- app/models/article.rb
+++ app/models/article.rb
@@ -36,7 +36,6 @@ class Article < ActiveRecord::Base
   delegate :terms, :cancellation, :about, :country , :to => :seller, :prefix => true
 
   # Relations
-  validates_presence_of :transaction, :unless => :template?
   belongs_to :transaction, :dependent => :destroy
   accepts_nested_attributes_for :transaction
 
@@ -51,7 +50,7 @@ class Article < ActiveRecord::Base
   # Misc mixins
   extend Sanitization
    # Article module concerns
-  include Categories, Commendation, FeesAndDonations, Images, Initial, Attributes, Search, Template, State, Scopes
+  include Categories, Commendation, FeesAndDonations, Images, BuildTransaction, Attributes, Search, Template, State, Scopes
 
   def images_attributes=(attributes)
     self.images.clear
@@ -124,5 +123,6 @@ class Article < ActiveRecord::Base
       "#{self.seller.company_name}, #{self.seller.city}"
     end
   end
+
 
 end
---
+++ db/migrate/20130814124951_add_default_values_for_articles.rb
@@ -0,0 +1,13 @@
+class AddDefaultValuesForArticles < ActiveRecord::Migration
+  def up
+    change_column :articles, :quantity, :integer, :default => 1
+    change_column :articles, :price_cents, :integer, :default => 100
+    change_column :articles, :friendly_percent, :integer, :default => 0, :limit => 3
+  end
+
+  def down
+    change_column :articles, :quantity, :integer
+    change_column :articles, :price_cents, :integer
+    change_column :articles, :friendly_percent, :integer, :limit => 3
+  end
+end
