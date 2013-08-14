0f72b599918ddbb249aa62c227b58a230bd8851b
Konstantin K <konstantin.koss@gmail.com>
2013-08-15 00:14:16 +0200

    transaction emails, optional mesage field, and tests

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -22,7 +22,7 @@
 class ArticleMailer < ActionMailer::Base
   default from: $email_addresses['ArticleMailer']['default_from']
 
-  def report_article(article,user,text)
+  def report_article article, user, text
     @text = text
     @article = article
     @user = user
@@ -30,7 +30,7 @@ class ArticleMailer < ActionMailer::Base
     mail(to: $email_addresses['ArticleMailer']['report'], from: user.email, subject: "Article reported with ID: #{article.id}")
   end
 
-  def category_proposal(category_proposal)
+  def category_proposal category_proposal
     mail(to: $email_addresses['ArticleMailer']['category_proposal'], subject: "Category proposal: #{category_proposal}")
   end
 
