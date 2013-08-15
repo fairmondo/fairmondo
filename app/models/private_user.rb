40f75f3b0b9fe01a21c60ceda9ac8ba2173a0ec0
nina <nina.gaensdorfer@gmx.de>
2013-08-15 17:37:39 +0200

    Bug fix

--- app/models/private_user.rb
+++ app/models/private_user.rb
@@ -19,17 +19,7 @@
 #
 class PrivateUser < User
 
-  #
-  # We cannot validate on user directly else resend password bzw. reset passwort does not work
-  # if the user object doesnt validate and the user cannot reset his password!
-  #
-  # validates user
-  validates_presence_of :forename , :on => :update
-  validates_presence_of :surname , :on => :update
-  validates_presence_of :country , :on => :update
-  validates_presence_of :street , :on => :update
-  validates_presence_of :city , :on => :update
-  validates_presence_of :zip , :on => :update
+
 
   # see http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
   def self.model_name
