f94e253a2d3d32f291b60ae853fe99fbbe7f8418
nina <nina.gaensdorfer@gmx.de>
2013-08-15 11:13:57 +0200

    Bring release branch up to date

--- app/models/article/build_transaction.rb
+++ app/models/article/build_transaction.rb
@@ -17,29 +17,15 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Farinopoly.  If not, see <http://www.gnu.org/licenses/>.
 #
-module Article::Initial
+module Article::BuildTransaction
   extend ActiveSupport::Concern
 
   included do
-   after_initialize :initialize_values
-   after_initialize :build_dependencies
+    before_create :build_specific_transaction, :unless => :template?
   end
 
-  def initialize_values
-    begin
-    self.quantity ||= 1
-    self.price ||= 1
-    self.friendly_percent ||= 0
-    # Auto-Completion raises MissingAttributeError:
-    # http://www.tatvartha.com/2011/03/activerecordmissingattributeerror-missing-attribute-a-bug-or-a-features/
-    #rescue ActiveModel::MissingAttributeError
-    end
-  end
-
-  def build_dependencies
-    self.build_transaction unless self.transaction
-    self.build_fair_trust_questionnaire unless self.fair_trust_questionnaire
-    self.build_social_producer_questionnaire unless self.social_producer_questionnaire
+  def build_specific_transaction
+    self.transaction = PreviewTransaction.create
   end
 
 
