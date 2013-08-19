82b76cfe3b0f74f1bdc8dd2e1efb1ec6fd1258ad
chiramiso <chiramiso@yahoo.de>
2013-08-19 09:30:18 +0200

    monday morning release before checkout

--- app/models/transaction_observer.rb
+++ app/models/transaction_observer.rb
@@ -1,6 +1,6 @@
 class TransactionObserver < ActiveRecord::Observer
 
-	def after_save(transaction)
+	def after_update(transaction)
 		if transaction.state == "sold"
 			invoice(transaction)
 		end
@@ -9,6 +9,6 @@ class TransactionObserver < ActiveRecord::Observer
 	def invoice(transaction)
 		@invoice = Invoice.new :user_id => transaction.seller.id, :invoice_date => 14.days.from_now
 	end
-	handle.asynchronously :invoice
+	handle_asynchronously :invoice
 
 end
