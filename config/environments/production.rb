36180dfc752fa5b3291a9ed71d33872baf8beea0
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-08-23 16:07:28 +0200

    url change #429

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Fairnopoly::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'beta.fairnopoly.de' }
+  config.action_mailer.default_url_options = { :host => 'www.fairnopoly.de' }
   # Enable threaded mode
   # config.threadsafe!
 
