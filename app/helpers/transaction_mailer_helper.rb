4f60d868830931e7abddcbf2f451869c0c127dbe
chiramiso <chiramiso@yahoo.de>
2013-09-13 12:28:41 +0200

    little email fix

--- app/helpers/transaction_mailer_helper.rb
+++ app/helpers/transaction_mailer_helper.rb
@@ -5,9 +5,9 @@ module TransactionMailerHelper
   def transaction_mail_greeting transaction, role
     case role
       when :buyer
-        t('transaction.notifications.greeting') + ' ' + @transaction.buyer_forename + ','
+        t('transaction.notifications.greeting') + ' ' + transaction.buyer_forename + ','
       when :seller
-        t('transaction.notifications.greeting') + ' ' + @transaction.article_seller_forename + ','
+        t('transaction.notifications.greeting') + ' ' + transaction.article_seller_forename + ','
     end
   end
 
