fb15594b6b84a63fdc90125b30ea207365b0884f
KonstantinKo <konstantin.koss@gmail.com>
2013-09-21 15:56:18 +0200

    fix missing User#direct_debit permission

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
     [:email, :password, :password_confirmation, :remember_me, :type,
     :nickname, :forename, :surname, :privacy, :legal, :agecheck, :paypal_account,
     :invitor_id, :banned, :about_me, :bank_code, #:trustcommunity,
-    :title, :country, :street, :city, :zip, :phone, :mobile, :fax,
+    :title, :country, :street, :city, :zip, :phone, :mobile, :fax, :direct_debit
     :bank_account_number, :bank_name, :bank_account_owner, :company_name,
     { image_attributes: Image.image_attrs }
     ]
