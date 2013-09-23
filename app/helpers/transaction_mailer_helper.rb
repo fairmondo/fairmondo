746168a6807b27607b4028c9022a1d6fdd6878be
chiramiso <chiramiso@yahoo.de>
2013-09-23 15:46:53 +0200

    fixed list_transactions (again)

--- app/helpers/transaction_mailer_helper.rb
+++ app/helpers/transaction_mailer_helper.rb
@@ -13,7 +13,7 @@ module TransactionMailerHelper
 
   def fairnopoly_email_footer
     "#{ t('common.fn_legal_footer.intro')}\n" +
-    "**************************************************************\n" +
+    "******************************************************************\n" +
     "#{t('common.fn_legal_footer.footer_contact')}\n\n" +
     "#{t('common.fn_legal_footer.registered')}\n" +
     "#{t('common.fn_legal_footer.board')}\n" +
@@ -22,7 +22,7 @@ module TransactionMailerHelper
     "#{t('common.claim')}\n\n" +
     "#{t('common.fn_legal_footer.facebook')}\n" +
     "#{t('common.fn_legal_footer.buy_shares')}\n" +
-    "**************************************************************"
+    "******************************************************************"
   end
 
   def show_contact_info_seller seller
@@ -145,12 +145,13 @@ module TransactionMailerHelper
     calc_fee = transaction.article.calculated_fee * transaction.quantity_bought
     calc_fair = transaction.article.calculated_fair * transaction.quantity_bought
     calc_total = calc_fee + calc_fair
+    VAT = 19
 
     "#{ t('transaction.notifications.seller.fees') }" + "#{ humanized_money_with_symbol( calc_fee ) }\n" +
     "#{ t('transaction.notifications.seller.donations') }" + "#{ humanized_money_with_symbol( calc_fair ) }\n" +
     "-------------------------------\n" +
-    "#{ t('transaction.edit.total_price') }" + "#{humanized_money_with_symbol( calc_total ) }" + "*" +
-    "#{ t('transaction.edit.net') }" + "#{ humanized_money_with_symbol( net calc_total) }" + "#{ t('transaction.edit.vat') }" + "#{ humanized_money_with_symbol( vat calc_total ) }"
+    "#{ t('transaction.edit.total_price') }" + "#{humanized_money_with_symbol( calc_total ) }" + "*\n" +
+    "#{ t('transaction.edit.net') }" + "#{ humanized_money_with_symbol( net( calc_total)) }" + "#{ t('transaction.edit.vat', percent: VAT) }" + "#{ humanized_money_with_symbol( vat(calc_total)) }"
   end
 
   def net price
