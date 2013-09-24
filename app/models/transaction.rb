773d382ead2a1a2bcec0515c76273c767ab4f25a
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-09-24 16:23:10 +0200

    fixed a bug with basic price with no issue

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -42,7 +42,7 @@ class Transaction < ActiveRecord::Base
 
   delegate :title, :seller, :selectable_transports, :selectable_payments,
            :transport_provider, :transport_price, :payment_cash_on_delivery_price,
-           :basic_price, :basic_price_amount, :price, :vat, :vat_price,
+           :basic_price, :basic_price_amount, :basic_price_amount_text, :price, :vat, :vat_price,
            :price_without_vat, :total_price, :quantity, :quantity_left,
            :transport_type1_provider, :transport_type2_provider, :calculated_fair,
            :custom_seller_identifier,
