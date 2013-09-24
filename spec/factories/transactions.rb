38a57a40e17c07e16d069f2536725162b8fe9153
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-09-24 11:43:21 +0200

    Fix test

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -73,6 +73,7 @@ FactoryGirl.define do
       state 'sold'
       selected_transport 'pickup'
       selected_payment 'cash'
+      sold_at { Time.now }
     end
 
     #factory :auction_transaction, :class => 'AuctionTransaction' do
