1a1e7b8a82f8692feae896fcfe3dee156bd701fa
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-10-08 18:16:42 +0200

    Refactoring

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -2,7 +2,7 @@ class ExportsController < ApplicationController
 
   def show
     respond_to do |format|
-      format.csv { send_data Article.export_articles(current_user,
+      format.csv { send_data Article::Export.export_articles(current_user,
                    params[:kind_of_article]),
                    {filename: "Fairnopoly_export_#{Time.now.strftime("%Y-%d-%m %H:%M:%S")}.csv"} }
     end
