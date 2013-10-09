500284ce77a5bdae0b32d4388066794c55cc4f37
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-10-09 16:16:39 +0200

    Further refactor import

--- app/models/mass_upload/questionnaire.rb
+++ app/models/mass_upload/questionnaire.rb
@@ -22,22 +22,20 @@
 module MassUpload::Questionnaire
   extend ActiveSupport::Concern
 
-  # Questionnaire.include_fair_questionnaires!(row_hash)
-
-  def self.include_fair_questionnaires!(row_hash)
+  def self.include_fair_questionnaires(row_hash)
     ftq_attributes = FairTrustQuestionnaire.new.attributes
     ftq_attributes.except!(*["id", "article_id"])
     spq_attributes = SocialProducerQuestionnaire.new.attributes
     spq_attributes.except!(*["id", "article_id"])
+    ftq = row_hash.extract!(*ftq_attributes.keys)
+    spq = row_hash.extract!(*spq_attributes.keys)
 
     if row_hash["fair_kind"] == "fair_trust"
-      row_hash["fair_trust_questionnaire_attributes"] = row_hash.extract!(*ftq_attributes.keys)
-      transform_questionnaire_attributes(row_hash["fair_trust_questionnaire_attributes"])
+      row_hash["fair_trust_questionnaire_attributes"] = transform_questionnaire_attributes(ftq)
     elsif row_hash["fair_kind"] == "social_producer"
-      row_hash["social_producer_questionnaire_attributes"] = row_hash.extract!(*spq_attributes.keys)
-      transform_questionnaire_attributes(row_hash["social_producer_questionnaire_attributes"])
+      row_hash["social_producer_questionnaire_attributes"] = transform_questionnaire_attributes(spq)
     end
-    row_hash.except! *(ftq_attributes.keys+spq_attributes.keys)
+    row_hash
   end
 
   def self.transform_questionnaire_attributes(attributes)
@@ -50,9 +48,10 @@ module MassUpload::Questionnaire
         end
       end
     end
+    attributes
   end
 
-  def self.add_commendation(article)
+  def self.add_commendation!(article)
     if article.fair_kind
       article.fair = true
     end
@@ -67,38 +66,4 @@ module MassUpload::Questionnaire
       article.small_and_precious = true
     end
   end
-
-
-  #   fair_trust_questionnaire_attributes_array = row[28..47]
-  #   social_producer_questionnaire_attributes_array = row[48..54]
-  #   fair_trust_questionnaire_attributes = {}
-  #   social_producer_questionnaire_attributes = {}
-  #   row = row - fair_trust_questionnaire_attributes_array - social_producer_questionnaire_attributes_array
-  #   if row[26][1] == "fair_trust"
-  #     key = "fair_trust_questionnaire_attributes"
-  #     row = inject_questionnaire(fair_trust_questionnaire_attributes_array, fair_trust_questionnaire_attributes, row, key)
-  #   elsif row[26][1] == "social_producer"
-  #     key = "social_producer_questionnaire_attributes"
-  #     row = inject_questionnaire(social_producer_questionnaire_attributes_array, social_producer_questionnaire_attributes, row, key)
-  #   else
-  #     row = Hash[row]
-  #   end
-  #   row
-  # end
-
-  # def inject_questionnaire (attributes_array, attributes_hash, row, key)
-  #   attributes_array.each do |pair|
-  #     if pair[0].include?("checkboxes") && pair[1..-1].first && pair[1..-1].first.split(',').length == 1
-  #       attributes_hash[pair.first] = [""] + pair[1].split
-  #     elsif pair[1..-1].first && cpair[1..-1].first.split(',').length > 1
-  #       attributes_hash[pair.first] = [""] + pair[1..-1].join.delete(' ').split(',')
-  #     else
-  #       attributes_hash[pair.first] = pair[1..-1].first || [""]
-  #     end
-  #   end
-  #   row = Hash[row]
-  #   row[key] = attributes_hash
-  #   row
-  # end
-
 end
