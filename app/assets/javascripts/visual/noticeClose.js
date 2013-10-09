d98535d79b9661e087363da72164c43c4e3718fc
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-10-09 11:14:49 +0200

    missing semicolons

--- app/assets/javascripts/visual/noticeClose.js
+++ app/assets/javascripts/visual/noticeClose.js
@@ -4,7 +4,7 @@ $(function() {
 
 	$('.Notice').on('click','.Notice-close', function(){
 		$(this).parent().parent().addClass('is-hidden');
-	})
+	});
 
 	// Hiding notices has no use
 	//$('.Notice').delay(5000).addClass('is-hidden');
