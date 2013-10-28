886e4571f652db453140b66a0137fa0d23560532
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-10-28 19:11:01 +0100

    mass upload threadsafeness

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Fairnopoly::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'www.fairnopoly.de' ,:protocol => 'https' }
   # Enable threaded mode
-  # config.threadsafe!
+  config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
