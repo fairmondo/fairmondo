9e60dcebc5f37fc19ebcab7043f7914b7d780851
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-10-29 14:31:46 +0100

    adding loading again

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -35,3 +35,5 @@ if Rails.env.development?
     end
   end
 end
+
+
--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -23,5 +23,21 @@ describe ExportsController do
         response.should be_success
       end
     end
+
+    describe "GET 'show' the errored articles" do
+      before do
+        article2 = FactoryGirl.create :article, seller: user
+        article2.images.first.update_attribute(:failing_reason => "test")
+      end
+      it "should be successful" do
+        time = Time.now
+        Time.stub(:now).and_return(time)
+        get :show, :kind_of_article => "error_articles", :format => "csv"
+        response.content_type.should eq("text/csv")
+        response.headers["Content-Disposition"].should eq("attachment; filename=\"Fairnopoly_export_#{time.strftime("%Y-%d-%m %H:%M:%S")}.csv\"")
+        response.should be_success
+      end
+    end
+
   end
 end
