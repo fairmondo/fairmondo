f5f0ab08276884a9b643d50555d24183a71e380e
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2013-11-12 16:03:32 +0100

    categorytopbar first #654

---
+++ app/assets/javascripts/visual/category.js
@@ -0,0 +1,6 @@
+$(function() {
+	$("#show-all-categories").hover(function() {
+		$(".Hero-categories").css("height","100%");
+		$(".HeaderArea").mouseleave(function() { $(".Hero-categories").css("height","1.5em"); });
+	});
+});
