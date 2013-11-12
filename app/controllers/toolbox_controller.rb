c57ab44a6c25c2f508aed03d5b25c86b543373ed
KonstantinKo <konstantin.koss@gmail.com>
2013-11-12 12:26:21 +0100

    expand messaging system - closes #658

--- app/controllers/toolbox_controller.rb
+++ app/controllers/toolbox_controller.rb
@@ -3,9 +3,9 @@ require 'rss'
 class ToolboxController < ApplicationController
   respond_to :js, :json
 
-  skip_before_filter :authenticate_user!, only: [ :session,:confirm,:rss ]
+  skip_before_filter :authenticate_user!, only: [ :session_expired,:confirm,:rss ]
 
-  def session
+  def session_expired
     respond_to do |format|
       #Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name) #for testing purposes
       format.json { render status: 200, json: { expired: current_user.nil? } }
@@ -34,11 +34,14 @@ class ToolboxController < ApplicationController
 
   # Send a single email to a private user, should be refactored when we have a real messaging system
   def contact
+    session[:seller_message] = {} unless session[:seller_message]
+    session[:seller_message][params[:contact][:article_id]] = params[:contact][:text] # store text
     return redirect_to :back, flash: { error: I18n.t('article.show.contact.acceptance_error') } unless params[:contact][:email_transfer_accepted] == "1" # manual validation: transfer of email was accepted
-    return redirect_to :back, flash: { error: I18n.t('article.show.contact.empty_error') } unless params[:contact][:text].length > 0 #manual validation: message is present
+    return redirect_to :back, flash: { error: I18n.t('article.show.contact.empty_error') } unless params[:contact][:text].length > 0 # manual validation: message is present
+    return redirect_to :back, flash: { error: I18n.t('article.show.contact.long_error') } unless params[:contact][:text].length < 2000 # manual validation: message is shorter than 2000 characters
     article = Article.find params[:contact][:article_id]
-    raise Pundit::NotAuthorizedError unless current_user || article.seller.is_a?(PrivateUser) #manual authorize
     ArticleMailer.contact(current_user.email, article.seller_email, params[:contact][:text], article).deliver
+    session[:seller_message][params[:contact][:article_id]] = nil # delöete from session
     redirect_to article, notice: I18n.t('article.show.contact.success_notice')
   end
 end
