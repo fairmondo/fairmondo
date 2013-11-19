9e67e8d2c170a2057006990bcb3fa11c1253ee4a
Schmidie <2toschmid@googlemail.com>
2013-11-19 17:24:54 +0100

    first version for fast queueing

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,5 +1,8 @@
 class ExhibitsController < InheritedResources::Base
   actions :create,:update
+  custom_actions :collection => :create_multiple
+  skip_after_filter :verify_authorized_with_exceptions, only: [:create_multiple]
+
 
   def create
     authorize build_resource
@@ -7,6 +10,26 @@ class ExhibitsController < InheritedResources::Base
       format.html {redirect_to resource.article}
     end
   end
+
+
+  def create_multiple
+    if params[:queue] && params[:articles]
+
+      queue = params[:queue]
+      articles = params[:articles]
+
+      articles.each do |id|
+        a = Article.find id
+        e = Exhibit.create({
+          article_id: a.id ,
+          queue: queue
+        })
+        e.save!
+      end
+    end
+    redirect_to root_path
+  end
+
 
   def update
     authorize resource
--- config/routes.rb
+++ config/routes.rb
@@ -102,7 +102,11 @@ Fairnopoly::Application.routes.draw do
 
   resources :categories, :only => [:show,:index]
 
-  resources :exhibits, :only => [:create,:update]
+  resources :exhibits, :only => [:create,:update] do
+    collection do
+      post 'create_multiple'
+    end
+  end
 
   root :to => 'welcome#index' # Workaround for double root https://github.com/gregbell/active_admin/issues/2049
 
