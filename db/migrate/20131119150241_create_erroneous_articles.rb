541f21631abefe461782930d7da542146ba5bc6f
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-11-20 14:19:52 +0100

    Start refactoring active record mass-upload

--- db/migrate/20131119150241_create_erroneous_articles.rb
+++ db/migrate/20131119150241_create_erroneous_articles.rb
@@ -2,6 +2,7 @@ class CreateErroneousArticles < ActiveRecord::Migration
   def change
     create_table :erroneous_articles do |t|
       t.integer :mass_upload_id
+      t.integer :row_index
       t.text :validation_errors
       t.text :article_csv
 
