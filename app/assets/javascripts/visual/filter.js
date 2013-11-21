264f121c42bd01e7739e7220fb6a11b337a92de9
KonstantinKo <konstantin.koss@gmail.com>
2013-11-21 15:01:15 +0100

    fix #693

--- app/assets/javascripts/visual/filter.js
+++ app/assets/javascripts/visual/filter.js
@@ -3,22 +3,24 @@
 //
 
 var toggleFilter = function(event){
-		event.preventDefault();
+  event.preventDefault();
 
-		var $filter = $('.Filter');
-		$filter.toggleClass('is-active');
-		$filter.find(".Filter-inner").css("overflow","hidden");
+  var $filter = $('.Filter');
+  $filter.toggleClass('is-active');
+  $filter.find(".Filter-inner").css("overflow","hidden");
 };
 
 $(function() {
-	var $filterToggle = $('.js-filter-toggle');
-	var $inputToggle = $('#search_input');
+  var $filterToggle = $('.js-filter-toggle');
+  var $inputToggle = $('#search_input');
 
-	$filterToggle.click(toggleFilter);
-	$inputToggle.focus(function() {
-		var $filter = $('.Filter');
-		$filter.addClass('is-active');
-	});
+  $filterToggle.click(toggleFilter);
+  $inputToggle.focus(function() {
+    var $filter = $('.Filter');
+    $filter.addClass('is-active');
+  });
 
+  // disable defaultly displayed advanced search box for mobile width
+  if ($(window).width() < 680)
+    $('.Hero-search .Filter').removeClass('is-active');
 });
-
