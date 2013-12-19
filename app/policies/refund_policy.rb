0b1e498f3d0a6ff397509b86b65e6c53741669ec
chiramiso <chiramiso@yahoo.de>
2013-12-19 14:02:12 +0100

    some additions to user.rb for getting updates of user profile to fastbill

--- app/policies/refund_policy.rb
+++ app/policies/refund_policy.rb
@@ -1,6 +1,6 @@
-class RefundPolicy < Struct.new(:user, :refund)
+class RefundPolicy < Struct.new( :user, :refund )
   def create?
-    own? && refund.transaction.sold? && !refund.transaction.requested_refund? 
+    own? && refund_possible? 
   end
   
   def new?
@@ -10,5 +10,16 @@ class RefundPolicy < Struct.new(:user, :refund)
   private
     def own?
       user.id == refund.transaction_seller.id
+    end
+
+    # this method checks if the transaction is in a state in which it is possible
+    # for it to request a refund
+    # The construction of the statement seems a bit odd, as it checks on an instance
+    # of Refund itself, this is possible as Refund.create creates a new instance which
+    # is not yet saved to the db and as such has no association with a transaction
+    # instance
+    #
+    def refund_possible?
+      refund.transaction.sold? && !refund.transaction.requested_refund?
     end
 end
