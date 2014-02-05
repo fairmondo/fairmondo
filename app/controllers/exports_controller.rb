3bd0fbd241cd2d6e75482776867d11494ecd672d
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-02-05 12:26:55 +0100

    changed export to work with chunks

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,9 +1,9 @@
 class ExportsController < ApplicationController
 
   def show
+     tempfile = Article::Export.export_articles(current_user, params[:kind_of_article])
      respond_to do |format|
-       format.csv { send_data Article::Export.export_articles(current_user,
-                    params[:kind_of_article]),
+       format.csv { send_file tempfile.path,
                    {filename: "Fairnopoly_export_#{Time.now.strftime("%Y-%d-%m %H:%M:%S")}.csv"} }
      end
 
