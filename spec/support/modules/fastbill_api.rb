2e12d5e6c3ec3dc7c63d1a0a5aeaea3e096155b4
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-02-10 18:10:00 +0100

    fastbill stubber now stubs more

--- spec/support/modules/fastbill_api.rb
+++ spec/support/modules/fastbill_api.rb
@@ -4,7 +4,8 @@ module FastBillStubber
     Fastbill::Automatic::Subscription.stub(:create).and_return(Fastbill::Automatic::Subscription.new(subscription_id: 1))
     Fastbill::Automatic::Subscription.stub(:setusagedata)
     Fastbill::Automatic::Subscription.stub(:get).and_return([Fastbill::Automatic::Subscription.new(subscription_id: 1)])
-    
+    Fastbill::Automatic::Customer.stub(:get).and_return([Fastbill::Automatic::Customer.new(customer_id: 1)])
+    Fastbill::Automatic::Customer.any_instance.stub(:update_attributes)
     # FastbillAPI.stub( :fee_wo_vat ).with( instance_of(Article) )
     # FastbillAPI.stub( :fair_wo_vat ).with( instance_of(Article) )
   end
