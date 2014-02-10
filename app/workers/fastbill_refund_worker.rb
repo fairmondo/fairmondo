7880e3608368303ab8e69ea916450520902cafdd
chiramiso <chiramiso@yahoo.de>
2014-02-10 15:39:44 +0100

    fixed refund worker

--- app/workers/fastbill_refund_worker.rb
+++ app/workers/fastbill_refund_worker.rb
@@ -8,6 +8,6 @@ class FastbillRefundWorker
   def perform transaction_id, fee_type
     transaction = Transaction.find(transaction_id)
     # Start the fastbill chain, to create invoices and add items to invoice
-    FastbillAPI.fastbill_refund( transaction, fee_type )
+    FastbillAPI.fastbill_refund( transaction, fee_type.to_sym )
   end
 end
