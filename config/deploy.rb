58eeaf98739455fd8b8d42f038f78ed926593195
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-03-03 16:15:01 +0100

    moved secret_token around

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :scm, :git
 # set :log_level, :debug
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/newrelic.yml config/actionmailer.yml config/api.yml config/email_addresses.yml config/secret_token.rb}
+set :linked_files, %w{config/database.yml config/newrelic.yml config/actionmailer.yml config/api.yml config/email_addresses.yml config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids public/system}
