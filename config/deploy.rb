54d31d45ef40c7089cf981f7fb435799f99fd391
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-03-03 16:44:56 +0100

    get a rails console on the worker server

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,3 +57,28 @@ namespace :deploy do
   after :finishing, "deploy:cleanup"
 
 end
+
+
+namespace :rails do
+  desc "Open the rails console on each of the remote servers"
+  task :console do
+    on roles(:console), :primary => true do |host| #does it for each host, bad.
+      rails_env = fetch(:stage)
+      execute_interactively "ruby #{current_path}/script/rails console #{rails_env}"
+    end
+  end
+
+  desc "Open the rails dbconsole on each of the remote servers"
+  task :dbconsole do
+    on roles(:db), :primary => true do |host| #does it for each host, bad.
+      rails_env = fetch(:stage)
+      execute_interactively "ruby #{current_path}/script/rails dbconsole #{rails_env}"
+    end
+  end
+
+  def execute_interactively(command)
+    user = fetch(:user)
+    port = fetch(:port) || 22
+    exec "ssh -l #{user} #{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :stage, :production
 server '78.109.61.168', user: 'deploy', roles: %w{web app db}
 server '78.109.61.169', user: 'deploy', roles: %w{web app}
 server '78.109.59.254', user: 'deploy', roles: %w{web app}
-server '5.22.150.182', user: 'deploy', roles: %w{sidekiq}
+server '5.22.150.182', user: 'deploy', roles: %w{sidekiq console}
 
 set :branch, ENV["BRANCH_NAME"] || "master"
 
