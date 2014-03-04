ced0f71380c4253a251629dd696967bb93549abf
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-03-04 15:25:36 +0100

    lowered log level for deployments and enabled dependency_loading for rake tasks

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,6 +77,8 @@ Fairnopoly::Application.configure do
 
   # Enable threaded mode
   config.threadsafe!
+  config.dependency_loading = true if $rails_rake_task
+  #http://stackoverflow.com/questions/4300240/rails-3-rake-task-cant-find-model-in-production
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
