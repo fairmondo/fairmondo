745a572ae25e2c7d00d96ce08c6ae30d4fab1732
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-03-04 14:01:57 +0100

    eager loading cleaned up

--- config/application.rb
+++ config/application.rb
@@ -42,10 +42,12 @@ module Fairnopoly
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib/autoload)
-    # Custom model loads
+
+    config.autoload_paths += Dir[Rails.root.join('lib','autoload', '{**}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
+    config.eager_load_paths += Dir[Rails.root.join('lib','autoload', '{**}')]
+    config.eager_load_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,6 @@ Fairnopoly::Application.configure do
   # Enable threaded mode
   config.threadsafe!
 
-  config.eager_load_paths += %W(#{config.root}/lib/autoload)
-  config.eager_load_paths += Dir[Rails.root.join('app', 'models', '{**}')]
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
