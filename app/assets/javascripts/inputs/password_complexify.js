ea9d42b025e21f7f4af0352a012a092682a286ab
Konstantin K <konstantin.koss@gmail.com>
2014-03-10 14:03:36 +0800

    fix #794 - easier registration

--- app/assets/javascripts/inputs/password_complexify.js
+++ app/assets/javascripts/inputs/password_complexify.js
@@ -22,16 +22,16 @@
 
 
 $(document).ready(function(){
-	$("#user_password").complexify( {strengthScaleFactor: 0.5} , function (valid, complexity) {
-			$("#password_complexity_progress").show();
-			$("#password_complexity_label").show();
-			var bar = $("#password_complexity_progress .bar");
-	    	bar.width( complexity + "%" );
-	    	if(valid) {
-	    		bar.addClass("bar-success").removeClass("bar-danger");
-	    	} else {
-	    		bar.addClass("bar-danger").removeClass("bar-success");
-	    	}
-	});
+  $("#user_password").complexify( {strengthScaleFactor: 0.5} , function (valid, complexity) {
+    $("#password_complexity_progress").show();
+    $("#password_complexity_label").show();
+    var bar = $("#password_complexity_progress .bar");
+    bar.width( complexity + "%" );
+    if(valid) {
+      bar.addClass("bar-success").removeClass("bar-danger");
+    } else {
+      bar.addClass("bar-danger").removeClass("bar-success");
+    }
+  });
 });
 
