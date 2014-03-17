273b4478d84c651aff630dfaa4f428d9bfe03459
Konstantin K <konstantin.koss@gmail.com>
2014-03-17 15:48:10 +0800

    fix #779 - implement default friendly_percent_organisation

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -175,4 +175,30 @@ module ArticlesHelper
     return [].to_json
   end
 
+  def build_category_table children, columns = 2
+    last = children.count - 1
+    last_in_column = columns - 1
+
+    output = '<table class="Category-dropdown-children">'
+    children.each_with_index do |child, index|
+      output += '<tr>' if index % columns == 0
+      output +=   '<td>'
+      output +=     "<a href='#{articles_path(article: {categories_and_ancestors: child.self_and_ancestors_ids})}'>"
+      output +=       child.name
+      output +=     '</a>'
+      output +=   '</td>'
+      output += '</tr>' if (index % columns) == last_in_column or index == last
+    end
+    output += '</table>'
+  end
+
+  def default_organisation_from organisation_list
+    begin
+      organisation_name = default_form_value('friendly_percent_organisation', resource)
+      default_organisation = organisation_list.select { |o| o.nickname == organisation_name }
+      default_organisation[0] ? default_organisation[0].id : nil
+    rescue
+      nil
+    end
+  end
 end
