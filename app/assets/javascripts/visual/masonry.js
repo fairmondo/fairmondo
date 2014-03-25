f379b65b83be97870c90ddc7fe89fbb080325272
Konstantin K <konstantin.koss@gmail.com>
2014-03-25 14:54:28 +0800

    for #811 - implement 2-5 grid

--- app/assets/javascripts/visual/masonry.js
+++ app/assets/javascripts/visual/masonry.js
@@ -5,24 +5,24 @@
 $(function(){
   var $container = $('.Grid-mansory');
   if ($container.length > 0) {
-    $container.prepend('<div class="gutter-sizer"></div><div class="grid-sizer"></div>');
+    $container.prepend('<div class="Category-GutterSizer"></div><div class="Category-GridSizer"></div>');
     $container.imagesLoaded( function() {
       $container.masonry({
-        itemSelector: '.Grid',
-        gutter: '.gutter-sizer',
-        columnWidth: '.grid-sizer'
+        itemSelector: '.Category-Grid',
+        gutter: '.Category-GutterSizer',
+        columnWidth: '.Category-GridSizer'
       });
     });
   }
 
   var $articles = $('.l-ArticleList');
   if ($articles.length > 0) {
-    $articles.prepend('<div class="gutter-sizer"></div><div class="grid-sizer"></div>');
+    $articles.prepend('<div class="Search-GutterSizer"></div><div class="Search-GridSizer"></div>');
     $articles.imagesLoaded( function() {
       $articles.masonry({
-        itemSelector: '.Grid',
-        gutter: '.gutter-sizer',
-        columnWidth: '.grid-sizer'
+        itemSelector: '.Search-Grid',
+        gutter: '.Search-GutterSizer',
+        columnWidth: '.Search-GridSizer'
       });
     });
   }
