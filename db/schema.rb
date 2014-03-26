94d6528a29f8e110a6b3e91b2b24e6dd0fcaabc2
Konstantin K <konstantin.koss@gmail.com>
2014-03-26 13:51:17 +0800

    merge fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20140319142030) do
 
   create_table "article_templates", :force => true do |t|
