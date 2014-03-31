57d01c5237e897951f12fe90d4872d9b9c50da11
Konstantin K <konstantin.koss@gmail.com>
2014-03-31 20:01:26 +0800

    for #811 - almost finished grid and dropdown. suite failing, needs css refactoring

--- app/objects/value/parent_category_tree.rb
+++ app/objects/value/parent_category_tree.rb
@@ -1,27 +1,44 @@
+# Object that returns values for category tree displays. Generally two levels deep.
+# Should have the same public API as Category.
 class ParentCategoryTree
-  attr_accessor :first_init
-
-  def initialize category = nil, first_init = true
+  # save the params and set up tree
+  def initialize category = nil, first_level = true
     @category = category
-    @first_init = first_init
+    @first_level = first_level
   end
 
+  # inherent name, name from param, or default
   def name
-    @category ? @category.name : "Kategorien"
+    current_category ? @category.name : "Alle Kategorien"
   end
 
+  # for link building with category_path(this): slug for show or empty string for index
   def to_s
     @category ? @category.slug : ''
   end
   alias_method :id, :to_s
 
+  # gives tree leaves to compute next
   def children
-    if @first_init
+    if current_category && @first_level # if a category was given we need no defaulting magic
+      current_category.children # this can get more that 2 levels deep
+    elsif @first_level # no category given but we need a second level: get all roots
       Category.other_category_last.sorted_roots.all.map do |category|
         ParentCategoryTree.new category, false
       end
-    else
+    else # we are on level two and don't want to deliver a level three
       []
     end
   end
+
+  private
+    # If there is no second level, get the parent to be the first one
+    def current_category
+      @current_category ||=
+        if @first_level and @category and @category.children.empty?
+          @category.parent
+        else
+          @category
+        end
+    end
 end
