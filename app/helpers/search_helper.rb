2b076d8de219d36b8b9cc0bc38d78e5cdb9cd4f1
chiramiso <chiramiso@yahoo.de>
2014-04-03 11:06:52 +0200

    dropdown chancges title with selected option

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -20,17 +20,23 @@
 # along with Fairnopoly.  If not, see <http://www.gnu.org/licenses/>.
 #
 module SearchHelper
+
+  extend Memoist
+
   def humanize_money_or_cents money_or_cents
     money_or_cents = Money.new(money_or_cents) unless money_or_cents.is_a?(Money)
     humanized_money_with_symbol money_or_cents
   end
 
+
   # returns a merged object
   def search_params_merged_with object
-    # TODO Memoize
     object.merge! :category_id => params[:id] if params[:controller] == 'categories'
     (params[:article_search_form] || {}).merge object
   end
+
+  memoize :search_params_merged_with
+
 end
 
 
