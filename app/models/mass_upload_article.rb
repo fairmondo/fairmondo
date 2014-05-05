f30423f5d57b4bc974476651747236ff73d105ad
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-05-05 14:47:32 +0200

    mass_uploads now track status and are more robust

--- app/models/mass_upload_article.rb
+++ app/models/mass_upload_article.rb
@@ -3,4 +3,14 @@
 class MassUploadArticle < ActiveRecord::Base
   belongs_to :article
   belongs_to :mass_upload
+  default_scope order('row_index ASC')
+
+  def still_working_or_done?
+    status = Sidekiq::Status::status(self.process_identifier)
+    return false unless status
+    still_working = [:queued,:working, :failed].include? status
+    done = (status == :complete and self.article.present?)
+    still_working || done
+  end
+
 end
