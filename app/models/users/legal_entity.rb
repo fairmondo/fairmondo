b087712b498703edf56f204b03bee55e506dd29a
chiramiso <chiramiso@yahoo.de>
2014-05-06 14:44:02 +0200

    #623 changed time for refund

--- app/models/users/legal_entity.rb
+++ app/models/users/legal_entity.rb
@@ -88,7 +88,7 @@ class LegalEntity < User
   end
 
   def can_refund? transaction
-    Time.now >= transaction.sold_at + 14.days && Time.now <= transaction.sold_at + 45.days
+    Time.now <= transaction.sold_at + 45.days
   end
 
   # see http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
