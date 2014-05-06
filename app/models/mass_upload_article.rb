d36c04785dd7c907f96087963d90212a19bee970
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-05-06 12:32:53 +0200

    timeouts and sidekiq issues

--- app/models/mass_upload_article.rb
+++ app/models/mass_upload_article.rb
@@ -5,12 +5,8 @@ class MassUploadArticle < ActiveRecord::Base
   belongs_to :mass_upload
   default_scope order('row_index ASC')
 
-  def still_working_or_done?
-    status = Sidekiq::Status::status(self.process_identifier)
-    return false unless status
-    still_working = [:queued,:working, :failed].include? status
-    done = (status == :complete and self.article.present?)
-    still_working || done
+  def done?
+    self.article.present? || self.validation_errors.present?
   end
 
 end
