7d9bc8def82eafbfa7f3b36d792f523a9eea1d9e
Konstantin K <konstantin.koss@gmail.com>
2014-05-08 14:49:38 +0800

    start #844 (a) - integrate checkboxes. they don't sync to cleverreach yet.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140506141148) do
+ActiveRecord::Schema.define(:version => 20140508061307) do
 
   create_table "article_templates", :force => true do |t|
     t.string   "name"
@@ -446,6 +446,7 @@ ActiveRecord::Schema.define(:version => 20140506141148) do
     t.string   "iban"
     t.string   "bic"
     t.boolean  "vacationing",                                 :default => false
+    t.boolean  "newsletter",                                  :default => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
