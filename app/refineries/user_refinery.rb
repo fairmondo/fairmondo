00d9dabdf5502aee953db07828f289eea5fe6110
Konstantin K <konstantin.koss@gmail.com>
2014-05-08 14:49:38 +0800

    start #844 (a) - integrate checkboxes. they don't sync to cleverreach yet.

--- app/refineries/user_refinery.rb
+++ app/refineries/user_refinery.rb
@@ -4,7 +4,7 @@ class UserRefinery < ApplicationRefinery
     [
       :email, :password, :password_confirmation, :new_terms_confirmed,
       # and custom fields apart from devise internal stuff:
-      :nickname, :type, :agecheck, :legal, :privacy
+      :nickname, :type, :agecheck, :newsletter, :legal, :privacy
     ]
   end
 
@@ -16,7 +16,7 @@ class UserRefinery < ApplicationRefinery
       :invitor_id, :banned, :about_me, :bank_code, #:trustcommunity,
       :title, :country, :street, :address_suffix, :city, :zip, :phone, :mobile, :fax, :direct_debit,
       :bank_account_number, :bank_name, :bank_account_owner, :company_name, :max_value_of_goods_cents_bonus,
-      :fastbill_profile_update, :vacationing,
+      :fastbill_profile_update, :vacationing, :newsletter,
       :iban,:bic,
       { image_attributes: ImageRefinery.new(Image.new, user).default }
     ]
