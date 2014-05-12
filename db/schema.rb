7e6323416411cd24fd1c7dab31d02466897e26f0
chiramiso <chiramiso@yahoo.de>
2014-05-12 09:33:27 +0200

    moved buyer message to the top of email for better visibility

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507144137) do
+ActiveRecord::Schema.define(version: 20140508061307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -438,6 +438,7 @@ ActiveRecord::Schema.define(version: 20140507144137) do
     t.string   "iban"
     t.string   "bic"
     t.boolean  "vacationing",                              default: false
+    t.boolean  "newsletter",                               default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
