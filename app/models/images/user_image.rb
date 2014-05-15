26353e21ca180812e1c57c19daa99f4a8605f98a
paul <paul@devhub.(none)>
2014-05-15 13:34:25 +0200

    .gitignore

--- app/models/images/user_image.rb
+++ app/models/images/user_image.rb
@@ -2,7 +2,7 @@ class UserImage < Image
   extend STI
   has_attached_file :image, styles: { original: "300>x300>", profile: "300x300>" },
                           convert_options: { profile: "-quality 75 -strip -background white -gravity center -extent 300x300" },
-                          default_url: ActionController::Base.helpers.asset_path("missing.png"),
+                          default_url: ActionController::Base.helpers.asset_path("assets/missing.png"),
                           url: "/system/images/:id_partition/:style/:filename",
                           path: "public/system/images/:id_partition/:style/:filename",
                           only_process: [:profile]
@@ -11,5 +11,4 @@ class UserImage < Image
   validates_attachment_presence :image, :unless => :external_url
   validates_attachment_content_type :image,:content_type => ['image/jpeg', 'image/png', 'image/gif']
   validates_attachment_size :image, :in => 1..2.megabytes # the 1 means one byte, not one megabyte
-
 end
