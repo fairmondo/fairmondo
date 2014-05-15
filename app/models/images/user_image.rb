fb7efd7045f16e7f81a1bc84ae70cd4ee46e8b93
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-05-15 16:21:59 +0200

    fix

--- app/models/images/user_image.rb
+++ app/models/images/user_image.rb
@@ -2,7 +2,7 @@ class UserImage < Image
   extend STI
   has_attached_file :image, styles: { original: "300>x300>", profile: "300x300>" },
                           convert_options: { profile: "-quality 75 -strip -background white -gravity center -extent 300x300" },
-                          default_url: "tikkakakak",
+                          default_url: Proc.new { |image| image.default_url_digest } ,
                           path: "public/system/images/:id_partition/:style/:filename",
                           only_process: [:profile]
   belongs_to :user, foreign_key: "imageable_id"
