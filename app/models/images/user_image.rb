26587bc8dcd4b8d3072a05f4297f2e5e3ebe5ef6
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-05-15 16:45:09 +0200

    url fix

--- app/models/images/user_image.rb
+++ app/models/images/user_image.rb
@@ -3,6 +3,7 @@ class UserImage < Image
   has_attached_file :image, styles: { original: "300>x300>", profile: "300x300>" },
                           convert_options: { profile: "-quality 75 -strip -background white -gravity center -extent 300x300" },
                           default_url: "/assets/missing.png",
+                          url: "/system/images/:id_partition/:style/:filename",
                           path: "public/system/images/:id_partition/:style/:filename",
                           only_process: [:profile]
   belongs_to :user, foreign_key: "imageable_id"
