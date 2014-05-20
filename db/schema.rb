000353a9d9035c38cd6bbac8d31c1ce8b3803bf1
chiramiso <chiramiso@yahoo.de>
2014-05-20 16:44:36 +0200

    started with missing filters

--- db/schema.rb
+++ db/schema.rb
@@ -172,16 +172,16 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.integer  "percent"
     t.integer  "max_discounted_value_cents"
     t.integer  "num_of_discountable_articles"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "exhibits", force: true do |t|
     t.integer  "article_id",         limit: 8
     t.string   "queue"
     t.integer  "related_article_id", limit: 8
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "exhibition_date"
   end
 
@@ -251,7 +251,7 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.boolean  "image_processing"
   end
 
-  add_index "images", ["imageable_id", "type"], name: "index_images_on_imageable_id_and_imageable_type", using: :btree
+  add_index "images", ["imageable_id", "type"], name: "index_images_on_imageable_id_and_type", using: :btree
 
   create_table "libraries", force: true do |t|
     t.string   "name"
@@ -280,8 +280,8 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.integer  "mass_upload_id"
     t.integer  "article_id"
     t.string   "action"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "row_index"
     t.text     "validation_errors"
     t.text     "article_csv"
@@ -297,8 +297,8 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.integer  "row_count"
     t.text     "failure_reason"
     t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
@@ -314,8 +314,8 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.integer  "user_id"
     t.string   "path"
     t.string   "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "notices", ["user_id"], name: "index_notices_on_user_id", using: :btree
@@ -338,8 +338,8 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.text     "text"
     t.integer  "business_transaction_id", limit: 8
     t.integer  "rated_user_id",           limit: 8
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "ratings", ["business_transaction_id"], name: "index_ratings_on_business_transaction_id", using: :btree
@@ -349,8 +349,8 @@ ActiveRecord::Schema.define(version: 20140508061307) do
     t.string   "reason"
     t.text     "description"
     t.integer  "business_transaction_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "refunds", ["business_transaction_id"], name: "index_refunds_on_business_transaction_id", using: :btree
