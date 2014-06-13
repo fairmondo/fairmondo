dc3e77c250993b42b193c44c818b55b7c30db8a0
KonstantinKo <konstantin.koss@gmail.com>
2014-06-13 10:55:51 +0200

    finish off temp changes to rewrites

--- config/rewrites.rb
+++ config/rewrites.rb
@@ -9,7 +9,7 @@ class RewriteConfig
     },{
       method: :r301,
       from: /(.*)/,
-      to: 'https://www.fairnopoly.de/categories/weitere',
+      to: 'https://www.fairnopoly.de/categories/weitere-abf793c9-d94b-423c-947d-0d8cb7bbe3b9',
       if: /weitere\./i
     }]
   end
