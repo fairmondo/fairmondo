028f25c9f331b4b33370e8627ee4ad7ae1747ffa
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-07-02 15:16:51 +0200

    added forgotten files

---
+++ deferred/controller/business_transactions_controller.rb
@@ -0,0 +1,105 @@
+#
+#
+# == License:
+# Fairnopoly - Fairnopoly is an open-source online marketplace.
+# Copyright (C) 2013 Fairnopoly eG
+#
+# This file is part of Fairnopoly.
+#
+# Fairnopoly is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Fairnopoly is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Fairnopoly.  If not, see <http://www.gnu.org/licenses/>.
+#
+class BusinessTransactionsController < ApplicationController
+  respond_to :html
+
+  before_filter :set_business_transaction
+  before_filter :redirect_if_already_sold, only: [:edit, :update]
+  before_filter :redirect_if_not_yet_sold, only: :show, unless: :multiple?
+  before_filter :redirect_to_child_show, only: :show, if: :multiple?
+  before_filter :dont_cache
+  before_filter :set_params_on_business_transaction, if: -> { request.patch? }, only: [:edit, :update]
+
+  def edit
+    authorize @business_transaction
+    if request.patch?
+      render :step2 if @business_transaction.valid?
+    end
+  end
+
+  def show
+    authorize @business_transaction
+  end
+
+  def already_sold
+    authorize @business_transaction
+  end
+
+  def print_order_buyer
+    authorize @business_transaction
+    render :print_order_buyer, layout: false
+  end
+
+  def update
+
+    authorize @business_transaction
+    @business_transaction.checking_out = true
+
+    if @business_transaction.valid? && @business_transaction.buy
+      return respond_with @business_transaction
+    elsif tos_not_accepted?
+      render :step2
+    else
+      render :edit
+    end
+  end
+
+  private
+    ## show ##
+    def redirect_if_not_yet_sold
+      if @business_transaction.available? && ( !@business_transaction.buyer || !@business_transaction.buyer.is?(current_user) )
+        redirect_to edit_business_transaction_path(@business_transaction)
+      end
+    end
+
+    def redirect_to_child_show
+      if @business_transaction.children
+        child_transactions = @business_transaction.children.select { |c| c.buyer == current_user }
+        unless child_transactions.empty?
+          redirect_to business_transaction_path child_transactions[-1]
+        end
+      end
+    end
+
+    ## edit, update ##
+    def redirect_if_already_sold
+      redirect_to already_sold_business_transaction_path(@business_transaction) unless @business_transaction.available?
+    end
+
+    ## other ##
+    def multiple?
+      @business_transaction.multiple?
+    end
+
+    def set_business_transaction
+      @business_transaction = BusinessTransaction.find(params[:id])
+    end
+
+    def set_params_on_business_transaction
+      @business_transaction.assign_attributes(params.for(@business_transaction).refine)
+      @business_transaction.buyer_id = current_user.id
+    end
+
+    def tos_not_accepted?
+      @business_transaction.errors.keys.size == 1 && @business_transaction.errors.has_key?(:tos_accepted)
+    end
+end
---
+++ deferred/models/business_transactions/single_fixed_price_transaction.rb
@@ -0,0 +1,65 @@
+#
+#
+# == License:
+# Fairnopoly - Fairnopoly is an open-source online marketplace.
+# Copyright (C) 2013 Fairnopoly eG
+#
+# This file is part of Fairnopoly.
+#
+# Fairnopoly is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Fairnopoly is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Fairnopoly.  If not, see <http://www.gnu.org/licenses/>.
+#
+class SingleFixedPriceTransaction < BusinessTransaction
+  extend STI
+
+  # Validations for buyer address
+  # validates :forename, presence: true, on: :update
+  # validates :surname, presence: true, on: :update
+  # validates :street, presence: true, on: :update
+  # validates :city, presence: true, on: :update
+  # validates :zip, presence: true, on: :update
+  # validates :country, presence: true, on: :update
+
+  state_machine do
+    after_transition on: :buy, do: :set_article_sold
+  end
+
+  # Allow quantity_bought
+  def quantity_bought
+    read_attribute :quantity_bought
+  end
+  # Only one can be bought. It's the whole point.
+  def quantity_bought= number
+    super 1
+  end
+
+  # for having a parallel structure to MultipleFPT
+  def quantity_available
+    self.sold? ? 0 : 1
+  end
+
+  # This might be called on article update when quantity has changed to more than 1
+  def transform_to_multiple quantity
+
+    self.class.send :define_method, :quantity_available do
+      read_attribute :quantity_available
+    end
+    self.update_column(:quantity_available, quantity)
+    self.update_column(:type, 'MultipleFixedPriceTransaction')
+  end
+
+  private
+    def set_article_sold
+      self.article.sold_out
+    end
+end
