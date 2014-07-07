c753df155169e035f54db7ca4b95237a7861306c
KonstantinKo <konstantin.koss@gmail.com>
2014-07-07 17:04:19 +0200

    small fixes, inclusing cookie creation on login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703133113) do
+ActiveRecord::Schema.define(version: 20140707143018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,7 +133,7 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id",    limit: 8
-    t.boolean  "sold"
+    t.boolean  "sold",                 default: false
   end
 
   add_index "carts", ["user_id"], name: "index_carts_on_user_id", using: :btree
@@ -173,16 +173,16 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.integer  "percent"
     t.integer  "max_discounted_value_cents"
     t.integer  "num_of_discountable_articles"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "exhibits", force: true do |t|
     t.integer  "article_id",         limit: 8
     t.string   "queue"
     t.integer  "related_article_id", limit: 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.datetime "exhibition_date"
   end
 
@@ -252,7 +252,7 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.boolean  "image_processing"
   end
 
-  add_index "images", ["imageable_id", "type"], name: "index_images_on_imageable_id_and_type", using: :btree
+  add_index "images", ["imageable_id", "type"], name: "index_images_on_imageable_id_and_imageable_type", using: :btree
 
   create_table "libraries", force: true do |t|
     t.string   "name"
@@ -307,8 +307,8 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.integer  "mass_upload_id"
     t.integer  "article_id"
     t.string   "action"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "row_index"
     t.text     "validation_errors"
     t.text     "article_csv"
@@ -324,8 +324,8 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.integer  "row_count"
     t.text     "failure_reason"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
@@ -341,8 +341,8 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.integer  "user_id"
     t.string   "path"
     t.string   "color"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "notices", ["user_id"], name: "index_notices_on_user_id", using: :btree
@@ -365,8 +365,8 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.text     "text"
     t.integer  "business_transaction_id", limit: 8
     t.integer  "rated_user_id",           limit: 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   add_index "ratings", ["business_transaction_id"], name: "index_ratings_on_business_transaction_id", using: :btree
@@ -376,8 +376,8 @@ ActiveRecord::Schema.define(version: 20140703133113) do
     t.string   "reason"
     t.text     "description"
     t.integer  "business_transaction_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   add_index "refunds", ["business_transaction_id"], name: "index_refunds_on_business_transaction_id", using: :btree
