c48d79db347393baf6b47136ead4a3ad98f7f0a8
Martin Meyerhoff <mamhoff@googlemail.com>
2014-07-10 09:42:51 +0200

    add destroy method, allow anonymous create

--- app/controllers/hearts_controller.rb
+++ app/controllers/hearts_controller.rb
@@ -1,33 +1,35 @@
 class HeartsController < ApplicationController
-  respond_to :html
-  skip_before_filter :authenticate_user!, only: [:create]
+  respond_to :js
+  skip_before_filter :authenticate_user!, only: :create
+
+  before_filter :set_library
 
   def create
     if user_signed_in?
-      @user = current_user
+      @heart = @library.hearts.build(user: current_user)
     else
-      @user = nil
+      user_token = generate_user_token(request.env['HTTP_USER_AGENT'], request.env['REMOTE_ADDR'])
+      @heart = @library.hearts.build(user_token: user_token)
     end
 
-    @library = Library.find(params[:library_id])
-    user_token = generate_user_token(request.env['HTTP_USER_AGENT'], request.env['REMOTE_ADDR'])
-    @heart = @library.hearts.build(user_token: user_token, user: @user)
     authorize @heart
-    #require 'pry'; binding.pry
-    respond_with @library, @heart do |format|
-      if @heart.save
-        format.js
-      else
-        format.js { render json: @heart.errors, status: :unprocessable_entity }
-      end
+    if !@heart.save
+      @message = "Jemand hat diese Sammlung schon von diesem Computer geherzt. Wenn du das nicht warst, logge dich bitte ein"
     end
   end
 
   def destroy
+    @heart = Heart.find(params[:id])
+    authorize @heart
+    @heart.destroy
   end
 
   private
     def generate_user_token(agent, addr)
       Digest::SHA2.hexdigest(agent + addr)
+    end
+
+    def set_library
+      @library = Library.find(params[:library_id])
     end
 end
