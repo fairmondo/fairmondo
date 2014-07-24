6f22154fd72c786da5f595004ff094cf662330ba
Martin Meyerhoff <mamhoff@googlemail.com>
2014-07-24 12:39:31 +0200

    remove unnecessary library association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -22,7 +22,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  belongs_to :library
 
   delegate :image_url, :nickname, to: :user, prefix: true
 
