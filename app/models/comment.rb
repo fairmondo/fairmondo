7234b63bc3925cb172f7014713ad945edb890ebb
Martin Meyerhoff <mamhoff@googlemail.com>
2014-07-28 14:12:12 +0200

    move counter_cache directive to from commentable to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,7 +21,7 @@
 #
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, counter_cache: true
 
   delegate :image_url, :nickname, to: :user, prefix: true
 
