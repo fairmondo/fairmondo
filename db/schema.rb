71f852222dfbea38478f027193d8f07464759e44
KonstantinKo <konstantin.koss@gmail.com>
2014-07-31 17:06:17 +0200

    implement comments for articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730094642) do
+ActiveRecord::Schema.define(version: 20140731122554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20140730094642) do
     t.string   "transport_time"
     t.boolean  "swappable",                                        default: false
     t.boolean  "borrowable",                                       default: false
+    t.integer  "comments_count",                                   default: 0
   end
 
   add_index "articles", ["created_at"], name: "index_articles_on_created_at", using: :btree
