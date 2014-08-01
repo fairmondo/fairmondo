ce1b54d7a6b64a0c17db897c2c154373f2ac72f6
chiramiso <chiramiso@yahoo.de>
2014-08-01 12:44:51 +0200

    paths of deleted images now written to file for backup purpose

--- app/observers/image_observer.rb
+++ app/observers/image_observer.rb
@@ -1,19 +1,8 @@
 class ImageObserver < ActiveRecord::Observer
 
-  # Save path to new images in text file for backup purpose
   def after_save(image)
-    if Rails.env == 'production'
-      arr = []
-      File.open("/var/www/fairnopoly/shared/backup_info/images_#{Time.now.strftime('%Y_%m_%d')}", 'a') do |file|
-        image.image.styles.each_key do |style|
-          unless arr.include?(style)
-            file.write(image.image.path(style) + "\n")
-            arr << style
-          end
-        end
-      end
-    end
-    true
+    # Save path to new images in text file for backup purpose
+    image.write_path_to_file_for('additions')
   end
 
 end
