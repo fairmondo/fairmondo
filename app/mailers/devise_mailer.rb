1ed9e7624e1af45681ec3d8636476d251781643d
chiramiso <chiramiso@yahoo.de>
2014-08-04 10:27:44 +0200

    finished implementing new layout for all emails

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -34,7 +34,7 @@ class DeviseMailer < Devise::Mailer
 
   end
 
-  def confirmation_instructions(record,token, opts={})
+  def confirmation_instructions(record, token, opts={})
 
     attachments['AGB_Fairnopoly.pdf'] = File.read(Rails.root.join('app/assets/docs/AGB_Fairnopoly_FINAL.pdf'))
     attachments['Datenschutz_Fairnopoly.pdf'] = File.read(Rails.root.join('app/assets/docs/Datenschutz_Fairnopoly_FINAL.pdf'))
