5f5b399a6c13f962b72b40eed07a983e66bdd6b4
Philipp Hansch <dev@phansch.net>
2014-08-05 13:52:37 +0200

    Add basic comments mailer

---
+++ app/mailers/comment_mailer.rb
@@ -0,0 +1,8 @@
+class CommentMailer < ActionMailer::Base
+  def report_comment_on_library(comment, commentable_owner, text)
+    @commentable = comment.commentable
+    @commentable_owner = commentable_owner
+
+    mail(to: @commentable_owner.email, subject: I18n.t('comment.new_notification'))
+  end
+end
