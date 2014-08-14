b30b35c8857b1c374f1f3ed71b2679bf94ea8649
Marc Altmann <mail@marcaltmann.com>
2014-08-14 19:06:46 +0200

    Changed popularity algorithm for libraries

--- app/workers/library_popularity_worker.rb
+++ app/workers/library_popularity_worker.rb
@@ -11,10 +11,26 @@ class LibraryPopularityWorker
   end
 
   def popularity_for library
-    # number of hearts in the last 30 days, each heart counts for one popularity point
-    popularity = library.hearts.where("updated_at > ? AND updated_at < ?", Time.now - 3.days, Time.now).count
-    # number of comments in the last 30 days, each comment counts ten times as much as a heart
-    popularity += library.comments.where("updated_at > ? AND updated_at < ?", Time.now - 3.days, Time.now).count * 10
-    popularity
+    # Time since last update
+    time_since_update = Time.now - library.updated_at
+    # Libraries that have been updated recently carry more weight
+    recency_factor = case time_since_update
+      when 0.days..3.days
+        10
+      when 3.days..7.days
+        2
+      else
+        1
+      end
+    # Alternative: recency_factor = 10 / time_since_update
+    # Hearts: Extra points for recent hearts
+    num_hearts = library.hearts.count
+    num_recent_hearts = library.hearts.where("updated_at > ? AND updated_at < ?", Time.now - 3.days, Time.now).count
+    # Comments: Extra points for recent comments
+    num_comments = library.comments.count
+    num_recent_comments = library.comments.where("updated_at > ? AND updated_at < ?", Time.now - 3.days, Time.now).count
+    # Calculate popularity
+    popularity = recency_factor * (num_hearts + num_recent_hearts * 2 + num_comments * 5 + num_recent_comments * 10)
+
   end
 end
