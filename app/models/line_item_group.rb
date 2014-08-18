f2870cb00f252523add527a8eaf985d18e7ad817
KonstantinKo <konstantin.koss@gmail.com>
2014-08-18 12:43:15 +0200

    bugfix: transport was nil if no unified transport was selected

--- app/models/line_item_group.rb
+++ app/models/line_item_group.rb
@@ -56,7 +56,7 @@ class LineItemGroup < ActiveRecord::Base
   end
 
   def unified_payment_method= value
-    super value
+    super
     self.business_transactions.each do |bt|
       bt.selected_payment = value if self.unified_payment?
     end
@@ -64,7 +64,7 @@ class LineItemGroup < ActiveRecord::Base
 
   def unified_transport= value
     super
-    self.business_transactions.each{ |bt| bt.selected_transport = nil } if value
+    self.business_transactions.each{ |bt| binding.pry; bt.selected_transport = nil } if self.unified_transport?
   end
 
   private
