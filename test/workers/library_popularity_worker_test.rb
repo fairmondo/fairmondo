f2ac0ee10b201794757f527218e629bf095b4f63
Marc Altmann <mail@marcaltmann.com>
2014-08-22 17:42:39 +0200

    Added controller and feature tests for trending libraries functionality

---
+++ test/workers/library_popularity_worker_test.rb
@@ -0,0 +1,10 @@
+
+require "test_helper"
+
+describe LibraryPopularityWorker do
+  it "should set library popularity field correctly" do
+    #Sidekiq.logger.stubs(:warn)
+    #mu = FactoryGirl.create :mass_upload
+    #ProcessRowMassUploadWorker.sidekiq_retries_exhausted_block.call({"class" => Object.class , "args" => [mu.id,"lala",3], "error_message" => "snafu"})
+  end
+end
