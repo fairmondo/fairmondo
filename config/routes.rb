4da855fbe79796b81b904780583da14f5d0f0f23
Marc Altmann <mail@marcaltmann.com>
2014-08-26 21:03:43 +0200

    Integrated search plugin for Firefox

--- config/routes.rb
+++ config/routes.rb
@@ -103,6 +103,8 @@ Fairnopoly::Application.routes.draw do
 
   get "feed", to: 'welcome#feed', constraints: {format: 'rss'}
 
+  get "/opensearch", to: "application#opensearch"
+
   resources :feedbacks, :only => [:create,:new]
 
   #the user routes
