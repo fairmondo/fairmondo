c35911d144fb6e0ab668cdd12437af81854a1b77
chiramiso <chiramiso@yahoo.de>
2014-09-01 13:42:19 +0200

    code coverage for line_item_group_exporter

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,16 +3,19 @@ class ExportsController < ApplicationController
   def show
      # Generate a Tempfile for the download
      csv = Tempfile.new "export", encoding: 'utf-8'
+     export_type = ''
 
      if params && params[:kind_of_article] == 'seller_line_item_groups'
        LineItemGroupExporter.export(csv, current_user, params)
+       export_type = 'purchase'
      else
        ArticleExporter.export(csv, current_user, params[:kind_of_article])
+       export_type = 'article'
      end
 
      respond_to do |format|
        format.csv { send_file csv.path,
-                   { type: 'text/csv; charset=utf-8' , filename: "Fairnopoly_export_#{Time.now.strftime("%Y-%d-%m %H:%M:%S")}.csv"} }
+                   { type: 'text/csv; charset=utf-8' , filename: "Fairnopoly_#{export_type}_export_#{Time.now.strftime("%Y-%d-%m %H:%M:%S")}.csv"} }
      end
   end
 end
