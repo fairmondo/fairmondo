0c69f60885aa01b7ae9c98c11f68c3f6c774cee9
Marc Altmann <mail@marcaltmann.com>
2014-09-03 17:22:02 +0200

    Improved personalized section on welcome page and added feature test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -37,7 +37,7 @@ class WelcomeController < ApplicationController
 
     # Personalized section
     if current_user
-      @last_hearted_library = current_user.hearted_libraries.reorder('hearts.created_at DESC').first
+      @last_hearted_library = current_user.hearted_libraries.min_elem(3).reorder('hearts.created_at DESC').first
       @current_private_library = current_user.libraries.personal.min_elem(3).first
     end
   end
