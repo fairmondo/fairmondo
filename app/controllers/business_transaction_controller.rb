68bba724e4daa7ef200a0e1ca2b80d67924fc8ec
KonstantinKo <konstantin.koss@gmail.com>
2014-09-05 16:28:05 +0200

    fixes from migration testing session

--- app/controllers/business_transaction_controller.rb
+++
@@ -1,8 +0,0 @@
-class BusinessTransactionController < ApplicationController
-
-  def show
-    @business_transction = BusinessTransaction.find params[:id]
-    authorize @business_transaction
-    redirect_to line_item_group_path @business_transaction.line_item_group
-  end
-end
