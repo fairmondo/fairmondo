2ee781a206d02cb2b9498a5ef754b9c05999a312
KonstantinKo <konstantin.koss@gmail.com>
2014-09-05 16:28:05 +0200

    fixes from migration testing session

--- app/policies/business_transaction_policy.rb
+++
@@ -1,5 +0,0 @@
-class BusinessTransactionPolicy < Struct.new(:user, :business_transaction)
-  def show?
-    true # as it will only redirect you it will be checked in line_item_grouppolicy
-  end
-end
