280b5a57e43a0496ea2fb2788599e5a29ef11dd5
chiramiso <chiramiso@yahoo.de>
2014-09-08 09:08:42 +0200

    attempt to add article images to cart emails

--- app/mailers/cart_mailer.rb
+++ app/mailers/cart_mailer.rb
@@ -6,6 +6,14 @@ class CartMailer < ActionMailer::Base
   layout 'email'
 
   def buyer_email(cart)
+    cart.line_item_groups.each do |lig|
+      lig.business_transactions.each do |bt|
+        attachments.inline[bt.article_id.to_s] = {
+        data: File.read("#{ Rails.root }/public#{ bt.article.title_image_url(:thumb) }")
+                                                 }
+      end
+    end
+
     @cart = cart
     @buyer = cart.user
     @subject = "[Fairnopoly] #{ t('transaction.notifications.buyer.buyer_subject') } Einkauf Nr: #{ cart.id }"
@@ -14,10 +22,16 @@ class CartMailer < ActionMailer::Base
   end
 
   def seller_email(line_item_group)
+    line_item_group.business_transactions.each do |bt|
+      attachments.inline[bt.article_id.to_s] = {
+        data: File.read("#{ Rails.root }/public#{ bt.article.title_image_url(:thumb) }")
+                                               }
+    end
+
     @line_item_group = line_item_group
     @buyer = line_item_group.buyer
     @seller = line_item_group.seller
-    @subject = "[Fairnopoly] #{ t('transaction.notifications.seller.seller_subject') } Verkauf Nr: #{ line_item_group.id }"
+    @subject = "[Fairnopoly] #{ t('transaction.notifications.seller.seller_subject') } Verkauf Nr: #{ line_item_group.purchase_id }"
 
     mail(to: @seller.email, subject: @subject)
   end
