6093216973b29c31ac5ffe645da2fccb1e0e8f14
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-09-10 14:47:27 +0200

    fixed tests

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -84,7 +84,7 @@ describe RegistrationsController do
       user = FactoryGirl.create(:user)
       sign_in user
       Image.any_instance.expects(:save)
-      put :update, {user: {nickname: user.nickname, image_attributes: {} },  address: { first_name: "" }}# invalid params
+      put :update, {user: {nickname: user.nickname, image_attributes: FactoryGirl.attributes_for(:user_image) },  address: { first_name: "" }}# invalid params
     end
   end
 end
