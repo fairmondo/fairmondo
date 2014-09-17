4239e4d1b70842e149060508be8201359d466f16
Marc Altmann <mail@marcaltmann.com>
2014-09-17 17:37:31 +0200

    Work in progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 # https://github.com/plataformatec/devise/blob/master/app/controllers/devise/sessions_controller.rb
 class SessionsController < Devise::SessionsController
   skip_before_filter :check_new_terms
-
-  def new
-    render layout: "application_reduced"
-  end
+  layout proc { request.xhr? ? false : 'application_reduced' }, only: :new
 end
