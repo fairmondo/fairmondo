8e6e522f362e60d262493d4036b14e7878accd2f
chiramiso <chiramiso@yahoo.de>
2014-09-29 15:30:24 +0200

    experimental filename rename worker for fixing problems with images

---
+++ app/workers/file_normalizer_worker.rb
@@ -0,0 +1,34 @@
+class FileNormalizerWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :file_normalizer,
+                  retry: 5,
+                  backtrace: true
+
+  def perform article
+    article.images.each do |image|
+      if image_not_accesible(image)
+        path = "var/www/fairnopoly/shared/#{image.path(:cut_here)}"
+        index = path.index('cut_here') - 1
+        path = path[0..index]
+        files = Dir.glob("#{path}*/*")
+
+        files.each do |file|
+          file_logger.log(Assets::Filename.normalize(file[file.rindex('/') + 1, file.length]))
+          #File.rename(file, Assets::Filename.normalize(file[file.rindex('/') + 1, file.length]))
+        end
+      end
+    end
+  end
+
+  def self.file_logger
+    Logger.new("#{Rails.root}/public/log/filerename.log")
+  end
+
+  def self.image_not_accesible(image)
+    Paperclip.io_adapters.for(image.image).read
+    return false
+  rescue
+    return true
+  end
+end
