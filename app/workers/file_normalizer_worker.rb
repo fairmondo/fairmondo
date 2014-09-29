05a84723ee0d6b3eedb7280fd489c75e0edf3398
chiramiso <chiramiso@yahoo.de>
2014-09-29 17:05:27 +0200

    another fix for the filenormalizerworker

--- app/workers/file_normalizer_worker.rb
+++ app/workers/file_normalizer_worker.rb
@@ -10,7 +10,7 @@ class FileNormalizerWorker
 
     article.images.each do |image|
       if image_not_accessible(image)
-        path = "var/www/fairnopoly/shared/#{image.path(:cut_here)}"
+        path = "var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
         index = path.index('cut_here') - 1
         path = path[0..index]
         files = Dir.glob("#{path}*/*")
