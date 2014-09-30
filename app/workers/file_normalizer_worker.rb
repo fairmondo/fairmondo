20f62e130b1fe4a78fa27ccd2caead7d0651a0a3
chiramiso <chiramiso@yahoo.de>
2014-09-30 09:12:25 +0200

    now the file renamer hopefully works

--- app/workers/file_normalizer_worker.rb
+++ app/workers/file_normalizer_worker.rb
@@ -2,22 +2,21 @@ class FileNormalizerWorker
   include Sidekiq::Worker
 
   sidekiq_options queue: :file_normalizer,
-                  retry: 5,
-                  backtrace: true
+    retry: 5,
+    backtrace: true
 
   def perform article_id
     article = Article.find article_id
 
     article.images.each do |image|
       if image_not_accessible(image)
-        path = "var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
+        path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
         index = path.index('cut_here') - 1
         path = path[0..index]
         files = Dir.glob("#{path}*/*")
 
         files.each do |file|
-          #file_logger.log(Assets::Filename.normalize(file[file.rindex('/') + 1, file.length]))
-          File.rename(file, Assets::Filename.normalize(file[file.rindex('/') + 1, file.length]))
+          File.rename(file, file[0..file.rindex('/')] + Assets::Filename.normalize(file[file.rindex('/') + 1..-1]))
         end
       end
     end
