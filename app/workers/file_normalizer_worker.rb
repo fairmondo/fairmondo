08e52acee42652d897fc6c2a2548813ab32535e5
chiramiso <chiramiso@yahoo.de>
2014-09-30 22:02:52 +0200

    worker now renames image files to name that is set in image object

--- app/workers/file_normalizer_worker.rb
+++ app/workers/file_normalizer_worker.rb
@@ -8,14 +8,24 @@ class FileNormalizerWorker
   def perform article_id
     article = Article.find article_id
 
-    article.images.each do |image|
-      path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
-      index = path.index('cut_here') - 1
-      path = path[0..index]
-      files = Dir.glob("#{path}*/*")
+    unless article.title_image_present?
+      article.images.each do |image|
+        # get the path to the image
+        path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
 
-      files.each do |file|
-        File.rename(file, file[0..file.rindex('/')] + Assets::Filename.normalize(file[file.rindex('/') + 1..-1]))
+        # get the filename of the image as referenced in image object
+        orig_filename = path[rindex('/') + 1..-1]
+
+        # get the base directory before the style directories
+        index = path.index('cut_here') - 1
+        path = path[0..index]
+
+        # get the paths to the image files in the style directories
+        files = Dir.glob("#{path}*/*")
+
+        files.each do |file|
+          File.rename(file, file[0..file.rindex('/')] + orig_filename)
+        end
       end
     end
   end
