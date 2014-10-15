bc3c61337a9b14d866afb98a73b0df34389e8945
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-10-15 19:04:27 +0200

    test fix and pry-rescue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require "minitest/rails/capybara"
 require "minitest/pride"
 require "mocha/mini_test"
 require 'capybara/rails'
-
+require 'pry-rescue/minitest' unless ENV["CI"]
 require Rails.root.join('test/support/spec_helpers/coverage.rb')
 
 require File.expand_path("../../config/environment", __FILE__)
