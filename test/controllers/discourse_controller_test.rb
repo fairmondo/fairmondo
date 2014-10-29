809792631b537acd31e4af71d777d0fcc5a9c3ec
chiramiso <chiramiso@yahoo.de>
2014-10-29 11:27:59 +0100

    coverage for DiscourseController

--- test/controllers/discourse_controller_test.rb
+++ test/controllers/discourse_controller_test.rb
@@ -6,9 +6,32 @@ describe DiscourseController do
     assert_response :found
   end
 
-  it 'should redirect to discourse' do
-    get :sso, sso: 'bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGImbmFtZT1z%0AYW0mdXNlcm5hbWU9c2Ftc2FtJmVtYWlsPXRlc3QlNDB0ZXN0LmNvbSZleHRl%0Acm5hbF9pZD1oZWxsbzEyMw%3D%3D%0A',
-              sig: '1c884222282f3feacd76802a9dd94e8bc8deba5d619b292bed75d63eb3152c0b'
-    response.must redirect_to new_user_session_path
+  describe 'for a logged out user' do
+    it 'should redirect to login page' do
+      get :sso, sso: 'bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A',
+        sig: '46e749cd26dcabc84eed323ff31f830da674dc87c77a2fcb1b296f76402ea900'
+      response.must redirect_to new_user_session_path
+    end
+  end
+
+  describe 'for a logged in user' do
+    before do
+      @user = FactoryGirl.create(:user, email: 'test@test.com', nickname: 'samsam')
+      sign_in @user
+    end
+
+    it 'should redirect to discourse forum with right params' do
+      get :sso, sso: 'bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A',
+        sig: '46e749cd26dcabc84eed323ff31f830da674dc87c77a2fcb1b296f76402ea900'
+      secret = $DISCOURSE_SECRET
+      @sso = SingleSignOn.parse(request.query_string, secret)
+      @sso.email = @user.email
+      @sso.name = @user.fullname if @user.fullname
+      @sso.username = @user.nickname
+      @sso.external_id = @user.id
+      @sso.sso_secret = secret
+
+      response.must redirect_to @sso.to_url($DISCOURSE_URL)
+    end
   end
 end
