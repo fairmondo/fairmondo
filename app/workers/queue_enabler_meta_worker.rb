cb126ab377f828ffc597ea54b354cdc8fb79910b
KonstantinKo <konstantin.koss@gmail.com>
2014-11-12 18:09:33 +0100

    paperclip_background back again on night shift (too much load)

--- app/workers/queue_enabler_meta_worker.rb
+++ app/workers/queue_enabler_meta_worker.rb
@@ -8,7 +8,7 @@ class QueueEnablerMetaWorker
                   retry: 5,
                   backtrace: true
 
-  NIGHT_WORKER = ['file_normalizer'] #'paperclip_background'
+  NIGHT_WORKER = ['file_normalizer', 'paperclip_background']
 
   def perform
     NIGHT_WORKER.each do |queue_name|
