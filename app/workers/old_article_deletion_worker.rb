5e5500e121febdfa1d3d2326f01a7314ed8d5f50
KonstantinKo <konstantin.koss@gmail.com>
2014-11-26 13:50:17 +0100

    enable scheduled cleanup workers

--- app/workers/old_article_deletion_worker.rb
+++ app/workers/old_article_deletion_worker.rb
@@ -1,15 +1,15 @@
 class OldArticleDeletionWorker
   include Sidekiq::Worker
-  # include Sidetiq::Schedulable
+  include Sidetiq::Schedulable
 
-  # recurrence { weekly.day_of_week(2).hour_of_day(12) }
+  recurrence { weekly.day(:tuesday).hour_of_day(12) }
 
   sidekiq_options queue: :cleanup,
                   retry: 10,
                   backtrace: true
 
   def perform
-    ref_ids = BusinessTransaction.pluck(:article_id)
+    ref_ids = BusinessTransaction.pluck(:article_id) # could get dangerous in the future with more sales
     Article.where.not(id: ref_ids).where(state: :closed).where("updated_at <= ?", 1.month.ago).select(:id).find_in_batches(batch_size: 100) do |group|
       OldArticleDeletionWorker.delay(queue: :cleanup).do_delete(group.map(&:id))
     end
