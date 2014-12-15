65a9802ea56a178cf9718d2be5fc05b987e2a89f
chiramiso <chiramiso@yahoo.de>
2014-12-15 10:49:05 +0100

    payments controller: restricted courier notification to bts with selected_transport bike_courier

--- app/refineries/payment_refinery.rb
+++ app/refineries/payment_refinery.rb
@@ -3,4 +3,8 @@ class PaymentRefinery < ApplicationRefinery
   def default
     [:type, :pay_key, :line_item_group_id]
   end
+
+  def ipn_notification
+    [:txn_id, :payment_status, :receiver_email]
+  end
 end
