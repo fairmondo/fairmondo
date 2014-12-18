2d8b952b974ac231f1f36a59f45bcaec4cf3c8b8
chiramiso <chiramiso@yahoo.de>
2014-12-18 10:28:34 +0100

    changed courier initializer

--- config/initializers/courier.rb
+++ config/initializers/courier.rb
@@ -1,5 +1,10 @@
 begin
-  courier = YAML.load(File.read('/var/www/fairnopoly/shared/config/courier.yml'))
+  courier = {}
+  if Rails.env == 'production'
+    courier = YAML.load(File.read('/var/www/fairnopoly/shared/config/courier.yml'))
+  else
+    courier = YAML.load(File.read(File.expand_path(File.join(Rails.root, 'config', 'courier.yml'))))
+  end
   $courier = courier['courier']
 
 rescue
