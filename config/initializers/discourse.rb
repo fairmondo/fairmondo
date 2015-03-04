8cc2ae956132e00d32aea1b90cddd897c09bc34d
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2015-03-04 16:17:12 +0100

    switch to secrets.yml

---
+++ config/initializers/discourse.rb
@@ -0,0 +1,2 @@
+$DISCOURSE_SECRET = Rails.application.secrets.discourse_secret
+$DISCOURSE_URL    = Rails.application.secrets.discourse_url
---
+++ config/initializers/konto_api.rb
@@ -0,0 +1,2 @@
+KontoAPI::api_key = Rails.application.secrets.kontoapi_key
+KontoAPI::timeout = 10
