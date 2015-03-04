11b0123ed3975ac81e3cb8557126dbfa2f94a033
Konstantin K <konstantin.koss@gmail.com>
2015-03-04 11:44:42 +0100

    for #1333 - categories always regenerate slugs

--- app/models/category.rb
+++ app/models/category.rb
@@ -21,27 +21,51 @@
 #
 class Category < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :name, :use => [:slugged, :finders]
+  friendly_id :name, use: [:slugged, :finders]
 
+  acts_as_nested_set
+
+  # Associations
   has_and_belongs_to_many :articles
-  has_and_belongs_to_many :active_articles, :class_name => 'Article', :conditions => {:state => "active"}
+  has_and_belongs_to_many :active_articles, class_name: 'Article',
+                                            conditions: { state: 'active' }
   # There is no possible way to keep a counter cache here
 
-  belongs_to :parent , :class_name => 'Category', :counter_cache => :children_count
+  belongs_to :parent, class_name: 'Category', counter_cache: :children_count
 
-  delegate :name, to: :parent, prefix: true
+  # Validations
+  validates :name, presence: true, uniqueness: true
 
+  # Scopes
   CATEGORY_NAV_THRESHOLD = 38
 
   scope :sorted, -> { order(:name) }
   scope :roots, -> { where(parent_id: nil) }
-  scope :all_by_id, -> { order("id ASC") }
-  scope :other_category_last, -> { order("CASE WHEN name = 'Weitere' THEN 1 ELSE 0 END") }#internationalize!
-  scope :weighted, -> { where('weight >= ?', CATEGORY_NAV_THRESHOLD).order("weight IS NULL, weight desc") }
-  scope :other_category, -> { where(parent_id: nil, name: 'Weitere') } #internationalize!
-  scope :other_categories, -> { where('parent_id IS NULL AND weight < ? OR weight IS NULL', CATEGORY_NAV_THRESHOLD) }
+  scope :all_by_id, -> { order('id ASC') }
+  scope :other_category_last, -> {
+    order("CASE WHEN name = 'Weitere' THEN 1 ELSE 0 END")
+  } #i18n!
+  scope :weighted, -> {
+    where('weight >= ?', CATEGORY_NAV_THRESHOLD)
+      .order('weight IS NULL, weight desc')
+  }
+  scope :other_category, -> { where(parent_id: nil, name: 'Weitere') } #18n!
+  scope :other_categories, -> {
+    where 'parent_id IS NULL AND weight < ? OR weight IS NULL',
+          CATEGORY_NAV_THRESHOLD
+  }
 
-  acts_as_nested_set
+  # Callbacks
+
+  # Categories get created from rails_admin, and it seems not to trigger slug
+  # generation at all. This sets it so that slugs always get regenerated on save
+  def should_generate_new_friendly_id?
+    true
+  end
+
+  # Methods
+
+  delegate :name, to: :parent, prefix: true
 
   def sorted_children
     self.children.except(:order).other_category_last.sorted.includes(:children)
@@ -59,18 +83,10 @@ class Category < ActiveRecord::Base
     delete_if_no_active_articles sorted_children.to_a
   end
 
-#  def siblings_with_active_articles
-#    siblings = sorted_siblings.to_a
-#    delete_if_no_active_articles siblings
-#  end
-#
-#  def self_and_siblings_with_active_articles
-#    [self] + siblings_with_active_articles
-#  end
-
   private
     def delete_if_no_active_articles array
-      array.delete_if { |node| node.children.empty? && node.active_articles.empty? }
+      array.delete_if do |node|
+        node.children.empty? && node.active_articles.empty?
+      end
     end
-
 end
