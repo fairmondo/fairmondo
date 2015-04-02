4f494b0b08d908d8bd6aa7dd9d7a58828eb88d3d
chiramiso <chiramiso@yahoo.de>
2015-04-02 21:19:32 +0200

    minor change to belboon worker

--- app/workers/belboon_article_exporter_worker.rb
+++ app/workers/belboon_article_exporter_worker.rb
@@ -12,7 +12,11 @@ class BelboonArticleExporterWorker
                   retry: 5,
                   backtrace: true
 
-  IDS = YAML.load(File.read(File.expand_path(File.join( Rails.root, 'config', 'belboon_trackable_users.yml'))))[:users]
+  begin
+    IDS = YAML.load(File.read(File.expand_path(File.join( Rails.root, 'config', 'belboon_trackable_users.yml'))))[:belboon][:users]
+  rescue
+    puts 'belboon_trackable_users.yml not found'
+  end
 
   def perform
     IDS.each do |id|
