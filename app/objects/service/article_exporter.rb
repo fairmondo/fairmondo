80f1ebb4c181c64a89c8318f0d33df49c29424e3
chiramiso <chiramiso@yahoo.de>
2015-04-08 20:09:49 +0200

    refactored article_exporter

--- app/objects/service/article_exporter.rb
+++ app/objects/service/article_exporter.rb
@@ -1,73 +1,74 @@
 class ArticleExporter
+  @@csv_options = { col_sep: ";",  encoding: 'utf-8' }
+  @@export_attributes = MassUpload.article_attributes
 
-  @@csv_options = { col_sep: ";",  encoding: 'utf-8'}
+  class << self
+    def export( csv, user, params = nil )
+      #write the headers and set options for csv generation
+      csv.puts CSV.generate_line @@export_attributes, @@csv_options
 
-  def self.export( csv, user, params = nil )
-
-    # Generate proper headers and find out if we are messing with transactions
-    export_attributes = MassUpload.article_attributes
-
-    #write the headers and set options for csv generation
-    csv.puts CSV.generate_line export_attributes, @@csv_options
-
-
-    determine_articles_to_export( user, params ).find_each do |article|
-
-      row = Hash.new
-      row.merge!(provide_fair_attributes_for article)
-      row.merge!(article.attributes)
-      row["categories"] = article.categories.map { |c| c.id }.join(",")
-      row["external_title_image_url"] = article.images.first.external_url if article.images.first
-      row["image_2_url"] = article.images[1].external_url if article.images[1]
-      csv.puts CSV.generate_line export_attributes.map { |element| row[element] }, @@csv_options
-
-    end
-    csv.flush
-  end
-
-
-  def self.export_erroneous_articles erroneous_articles
-    csv = CSV.generate_line( MassUpload.article_attributes, @@csv_options )
-    erroneous_articles.each do |article|
-      csv += article.article_csv
-    end
-    csv
-  end
-
-
-  def self.determine_articles_to_export user, params
-    if params == "active"
-      user.articles.where(:state => "active").order("created_at ASC").includes(:images,:categories,:social_producer_questionnaire,:fair_trust_questionnaire)
-    elsif params == "inactive"
-      user.articles.where("state = ? OR state = ?","preview","locked").order("created_at ASC").includes(:images,:categories,:social_producer_questionnaire,:fair_trust_questionnaire)
-    end
-  end
-
-
-  def self.provide_fair_attributes_for article
-    attributes = Hash.new
-    if article.fair_trust_questionnaire
-      attributes.merge!(article.fair_trust_questionnaire.attributes)
+      determine_articles_to_export( user, params ).find_each do |article|
+        row = create_row_for article
+        write_row_to_csv csv, row
+      end
+      csv.flush
     end
 
-    if article.social_producer_questionnaire
-      attributes.merge!(article.social_producer_questionnaire.attributes)
+    def export_erroneous_articles erroneous_articles
+      csv = CSV.generate_line(@@export_attributes, @@csv_options)
+      erroneous_articles.each do |article|
+        csv += article.article_csv
+      end
+      csv
     end
-    serialize_checkboxes_in attributes
-  end
 
-
-  def self.serialize_checkboxes_in attributes
-    attributes.each do |k, v|
-      if k.include?("checkboxes")
-        if v.any?
-          attributes[k] = v.join(',')
-        else
-          attributes[k] = nil
+    private
+      def determine_articles_to_export user, params
+        if params == "active"
+          user.articles.where(:state => "active").order("created_at ASC")
+            .includes(:images, :categories, :social_producer_questionnaire, :fair_trust_questionnaire)
+        elsif params == "inactive"
+          user.articles.where("state = ? OR state = ?","preview","locked").order("created_at ASC")
+            .includes(:images, :categories, :social_producer_questionnaire, :fair_trust_questionnaire)
         end
       end
-    end
-    attributes
-  end
 
+      def create_row_for article
+        row = Hash.new
+        row.merge!(provide_fair_attributes_for article)
+        row.merge!(provide_social_attributes_for article)
+        row.merge!(article.attributes)
+        row["categories"] = article.categories.map { |c| c.id }.join(",")
+        row["external_title_image_url"] = article.images.first.external_url if article.images.first
+        row["image_2_url"] = article.images[1].external_url if article.images[1]
+        row
+      end
+
+      def write_row_to_csv csv, row
+        csv.puts CSV.generate_line @@export_attributes.map { |element| row[element] }, @@csv_options
+      end
+
+      def provide_fair_attributes_for article
+        attributes = Hash.new
+        if article.fair_trust_questionnaire
+          attributes.merge!(article.fair_trust_questionnaire.attributes)
+        end
+        serialize_checkboxes_in attributes
+      end
+
+      def provide_social_attributes_for article
+        attributes = Hash.new
+        if article.social_producer_questionnaire
+          attributes.merge!(article.social_producer_questionnaire.attributes)
+        end
+        serialize_checkboxes_in attributes
+      end
+
+      def serialize_checkboxes_in attributes
+        attributes.select{ |k, _v| k.include?('checkboxes') }.each do |k, v|
+          attributes[k] = v.any? ? v.join(',') : nil
+        end
+        attributes
+      end
+  end
 end
