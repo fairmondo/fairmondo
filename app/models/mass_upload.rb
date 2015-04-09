33bb820446fdc1258b62f7056940bc2b7665a949
chiramiso <chiramiso@yahoo.de>
2015-04-09 12:38:14 +0200

    pulling mass_upload model apart

--- app/models/mass_upload.rb
+++ app/models/mass_upload.rb
@@ -21,79 +21,9 @@
 #
 class MassUpload < ActiveRecord::Base
   include Assets::Normalizer
-
-  state_machine initial: :pending do
-    event :start do
-      transition pending: :processing
-    end
-
-    event :error do
-      transition processing: :failed
-      transition failed: :failed # maybe another worker calls it
-    end
-
-    event :finish do
-      transition processing: :finished, if: :can_finish?
-      transition finished: :finished
-    end
-
-    event :mass_activate do
-      transition finished: :activated
-      transition activated: :activated
-    end
-
-    after_transition to: :finished do |mass_upload, _transition|
-      ArticleMailer.delay.mass_upload_finished_message(mass_upload.id)
-    end
-
-    after_transition processing: :failed do |mass_upload, transition|
-      mass_upload.failure_reason = transition.args.first
-      mass_upload.save
-      ArticleMailer.delay.mass_upload_failed_message(mass_upload.id)
-    end
-
-    after_transition to: :activated do |mass_upload, _transition|
-      mass_upload.articles_for_mass_activation.update_all(state: 'active')
-      Indexer.delay_for(3.seconds).index_mass_upload mass_upload.id
-      ArticleMailer.delay.mass_upload_activation_message(mass_upload.id)
-    end
-  end
+  include Checks, Questionnaire, FeesAndDonations, Associations, State
 
   scope :processing, -> { where(state: :processing) }
-
-  include Checks, Questionnaire, FeesAndDonations
-
-  has_many :mass_upload_articles
-  has_many :articles, through: :mass_upload_articles
-
-  has_many :created_articles, -> { where('mass_upload_articles.action' => 'create') }, through: :mass_upload_articles, source: :article
-  has_many :updated_articles, -> { where('mass_upload_articles.action' => 'update') }, through: :mass_upload_articles, source: :article
-  has_many :deleted_articles, -> { where('mass_upload_articles.action' => 'delete') }, through: :mass_upload_articles, source: :article
-  has_many :deactivated_articles, -> { where('mass_upload_articles.action' => 'deactivate') }, through: :mass_upload_articles, source: :article
-  has_many :activated_articles, -> { where('mass_upload_articles.action' => 'activate') }, through: :mass_upload_articles, source: :article
-  has_many :articles_for_mass_activation, -> { where("mass_upload_articles.action IN ('create', 'update', 'activate')") }, through: :mass_upload_articles, source: :article
-  has_many :skipped_articles, -> { where('mass_upload_articles.action' => 'nothing') }, through: :mass_upload_articles, source: :article
-
-  has_many :valid_mass_upload_articles, -> { where(validation_errors: nil).where.not(article_id: nil) }, class_name: 'MassUploadArticle'
-  has_many :erroneous_articles, -> { where.not(validation_errors: nil) }, class_name: 'MassUploadArticle'
-
-  has_attached_file :file
-  belongs_to :user
-
-  delegate :nickname, to: :user, prefix: true
-
-  validates_attachment :file, presence: true,
-                              size: { in: 0..100.megabytes },
-                              content_type: {
-                                content_type: %w(
-                                  text/csv application/excel text/anytext
-                                  application/vnd.msexcel
-                                  application/vnd.ms-excel
-                                  application/octet-stream
-                                  application/force-download
-                                  text/comma-separated-values
-                                )
-                              }
 
   def self.article_attributes
     %w(
