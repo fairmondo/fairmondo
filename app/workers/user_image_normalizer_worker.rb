b3cf077f5d0234fbdf66a99ae0088843b79a1964
chiramiso <chiramiso@yahoo.de>
2015-04-20 14:25:10 +0200

    fix user image normalizer worker

--- app/workers/user_image_normalizer_worker.rb
+++ app/workers/user_image_normalizer_worker.rb
@@ -2,37 +2,36 @@ class UserImageNormalizerWorker
   include Sidekiq::Worker
 
   sidekiq_options queue: :file_normalizer,
-                  retry: 5,
-                  backtrace: true
+    retry: 5,
+    backtrace: true
 
   def perform id
     user = User.find id
+    image = user.image
 
-    user.images.each do |image|
-      # get the path to the image
-      path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
+    # get the path to the image
+    path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
 
-      # get the filename of the image as referenced in image object
-      orig_filename = path[path.rindex('/') + 1..-1]
+    # get the filename of the image as referenced in image object
+    orig_filename = path[path.rindex('/') + 1..-1]
 
-      # get the base directory before the style directories
-      index = path.index('cut_here') - 1
-      path = path[0..index]
+    # get the base directory before the style directories
+    index = path.index('cut_here') - 1
+    path = path[0..index]
 
-      # get the paths to the image files in the style directories
-      files = Dir.glob("#{path}original/*")
+    # get the paths to the image files in the style directories
+    files = Dir.glob("#{path}original/*")
 
-      old_filepath = files.first
-      old_filename = old_filepath[old_filepath.rindex('/') + 1..-1]
+    old_filepath = files.first
+    old_filename = old_filepath[old_filepath.rindex('/') + 1..-1]
 
-      unless orig_filename == old_filename
-        image.update_column :image_file_name, old_filename
-      end
-      # files.each do |file|
-      #   File.rename(file, file[0..file.rindex('/')] + orig_filename)
-      # end
-      # # index changed articles
-      # Indexer.index_article(article)
+    unless orig_filename == old_filename
+      image.update_column :image_file_name, old_filename
     end
+    # files.each do |file|
+    #   File.rename(file, file[0..file.rindex('/')] + orig_filename)
+    # end
+    # # index changed articles
+    # Indexer.index_article(article)
   end
 end
