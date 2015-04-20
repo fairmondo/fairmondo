caef54730ea5fce1bcd1156de88b28e1cb6a98f2
chiramiso <chiramiso@yahoo.de>
2015-04-20 14:36:40 +0200

    another user file normalizer fix

--- app/workers/user_image_normalizer_worker.rb
+++ app/workers/user_image_normalizer_worker.rb
@@ -7,31 +7,31 @@ class UserImageNormalizerWorker
 
   def perform id
     user = User.find id
-    image = user.image
+    if image = user.image
+      # get the path to the image
+      path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
 
-    # get the path to the image
-    path = "/var/www/fairnopoly/shared/#{image.image.path(:cut_here)}"
+      # get the filename of the image as referenced in image object
+      orig_filename = path[path.rindex('/') + 1..-1]
 
-    # get the filename of the image as referenced in image object
-    orig_filename = path[path.rindex('/') + 1..-1]
+      # get the base directory before the style directories
+      index = path.index('cut_here') - 1
+      path = path[0..index]
 
-    # get the base directory before the style directories
-    index = path.index('cut_here') - 1
-    path = path[0..index]
+      # get the paths to the image files in the style directories
+      files = Dir.glob("#{path}original/*")
 
-    # get the paths to the image files in the style directories
-    files = Dir.glob("#{path}original/*")
+      old_filepath = files.first
+      old_filename = old_filepath[old_filepath.rindex('/') + 1..-1]
 
-    old_filepath = files.first
-    old_filename = old_filepath[old_filepath.rindex('/') + 1..-1]
-
-    unless orig_filename == old_filename
-      image.update_column :image_file_name, old_filename
+      unless orig_filename == old_filename
+        image.update_column :image_file_name, old_filename
+      end
+      # files.each do |file|
+      #   File.rename(file, file[0..file.rindex('/')] + orig_filename)
+      # end
+      # # index changed articles
+      # Indexer.index_article(article)
     end
-    # files.each do |file|
-    #   File.rename(file, file[0..file.rindex('/')] + orig_filename)
-    # end
-    # # index changed articles
-    # Indexer.index_article(article)
   end
 end
