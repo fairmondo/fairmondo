a01d46d73a319254ec499d441371cee08ea17f8b
George Michokostas <michokostas@gmail.com>
2015-10-12 00:07:08 +0200

    Refactor model tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,9 +1,36 @@
 require 'test_helper'
 
 describe Cart do
-  let(:cart) { Cart.new }
+  subject { Cart.new }
+
+  describe 'attributes' do
+    it { subject.must_respond_to :sold }
+    it { subject.must_respond_to :line_item_count }
+    it { subject.must_respond_to :purchase_emails_sent }
+    it { subject.must_respond_to :purchase_emails_sent_at }
+    it { subject.must_respond_to :created_at }
+    it { subject.must_respond_to :updated_at }
+    it { subject.must_respond_to :user_id }
+    it { subject.must_respond_to :id }
+  end
 
   it 'must be valid' do
-    cart.must_be :valid?
+    subject.must_be :valid?
+  end
+
+  describe 'associations' do
+    it { subject.must belong_to :user }
+    it { subject.must have_many :line_item_groups }
+    it { subject.must have_many :line_items }
+    it { subject.must have_many :articles }
+  end
+
+  describe 'methods' do
+    describe '#empty?' do
+      it 'should not be empty' do
+        @cart = FactoryGirl.create :cart, :with_line_item_groups
+        @cart.empty?.must_equal false
+      end
+    end
   end
 end
