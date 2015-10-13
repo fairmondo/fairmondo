c8662bc792e370840ef79deb11cc6a910f289268
Marc Altmann <mail@marcaltmann.com>
2015-10-13 12:05:38 +0200

    Apply new legal comments to chewy, helpers and inputs files

--- app/helpers/statistic_helper.rb
+++ app/helpers/statistic_helper.rb
@@ -1,3 +1,7 @@
+#   Copyright (c) 2012-2015, Fairmondo eG.  This file is
+#   licensed under the GNU Affero General Public License version 3 or later.
+#   See the COPYRIGHT file for details.
+
 module StatisticHelper
   def statistic_tr name, value, growth = nil
     render 'statistic_tr', name: name, value: value, growth: growth
