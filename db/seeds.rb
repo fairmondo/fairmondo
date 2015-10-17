32a251e114ab388b906c964c13e6df708dcd183c
Marc Altmann <mail@marcaltmann.com>
2015-10-17 13:16:00 +0200

    Apply new legal comments to rest of files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,7 @@
-# encoding: utf-8
-#
-#
-# == License:
-# Fairmondo - Fairmondo is an open-source online marketplace.
-# Copyright (C) 2013 Fairmondo eG
-#
-# This file is part of Fairmondo.
-#
-# Fairmondo is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Fairmondo is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with Fairmondo.  If not, see <http://www.gnu.org/licenses/>.
-#
+#   Copyright (c) 2012-2015, Fairmondo eG.  This file is
+#   licensed under the GNU Affero General Public License version 3 or later.
+#   See the COPYRIGHT file for details.
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
--- lib/autoload/sti.rb
+++ lib/autoload/sti.rb
@@ -1,24 +1,8 @@
+#   Copyright (c) 2012-2015, Fairmondo eG.  This file is
+#   licensed under the GNU Affero General Public License version 3 or later.
+#   See the COPYRIGHT file for details.
+
 # This is a helper that is supposed to be extended by all single table inheritance subclasses.
-#
-# == License:
-# Fairmondo - Fairmondo is an open-source online marketplace.
-# Copyright (C) 2013 Fairmondo eG
-#
-# This file is part of Fairmondo.
-#
-# Fairmondo is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Fairmondo is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with Fairmondo.  If not, see <http://www.gnu.org/licenses/>.
-#
 module STI
   # See http://stackoverflow.com/questions/6146317/is-subclassing-a-user-model-really-bad-to-do-in-rails
   #
