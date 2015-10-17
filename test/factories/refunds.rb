684f97368e4a11b5ff304912df0e31e5620e09f4
Marc Altmann <mail@marcaltmann.com>
2015-10-17 20:43:19 +0200

    Refactor refund factory

--- test/factories/refunds.rb
+++ test/factories/refunds.rb
@@ -4,8 +4,12 @@
 
 FactoryGirl.define do
   factory :refund do
-    reason :not_in_stock
-    description 'ab' * 80
-    business_transaction { FactoryGirl.create :business_transaction, :old }
+    reason :not_paid
+    description 'We spent as much money as we could, and got as little for it as people could '\
+      'make up their minds to give us. We were always more or less miserable, and most of our '\
+      'acquaintance were in the same condition. There was a gay fiction among us that we were '\
+      'constantly enjoying ourselves, and a skeleton truth that we never did. To the best of my '\
+      'belief, our case was in the last aspect a rather common one.'
+    association :business_transaction, :old
   end
 end
