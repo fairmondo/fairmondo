ebbcf020e89e15cdeaf28b377baaa1ee25df0d48
Marc Altmann <mail@marcaltmann.com>
2016-04-27 12:43:40 +0200

    Migrate direct_debit field to new DirectDebitMandate records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408225826) do
+ActiveRecord::Schema.define(version: 20160427095019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
