5cbfc6e4ca06e25b51200268572ad0781cbe475a
Marc Altmann <mail@marcaltmann.com>
2016-10-24 10:56:16 +0200

    Change Sidekiq servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,11 +4,7 @@
 
 set :stage, :production
 
-server '78.109.61.168', user: 'deploy', roles: %w{web app db}
-server '78.109.61.169', user: 'deploy', roles: %w{web app}
-server '78.109.59.254', user: 'deploy', roles: %w{sidekiq}
-server '5.22.150.182', user: 'deploy', roles: %w{sidekiq console}
+server '78.109.61.168', user: 'deploy', roles: %w{web app db sidekiq}
+server '78.109.61.169', user: 'deploy', roles: %w{web app sidekiq console}
 
 set :branch, ENV['BRANCH_NAME'] || 'master'
-
-# set :sidekiq_processes, ->{ 2 }
