4f44443efa71cbffc75cec8408a7f7d11a70ff30
Marc Altmann <marcaltmann@posteo.de>
2016-12-17 15:52:23 +0100

    Hopefully fix bug where confirmation instructions are not sent

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -3,22 +3,5 @@
 #   See the COPYRIGHT file for details.
 
 class DeviseMailer < Devise::Mailer
-  # helper :application # gives access to all helpers defined within `application_helper`.
-  include MailerHelper
   layout 'text-email'
-
-  def welcome_mail(record, token, _opts = {})
-    attachments['AGB_Fairmondo.pdf'] = File.read(Rails.root.join('app/assets/docs/AGB.pdf'))
-    attachments['Datenschutz_Fairmondo.pdf'] = File.read(Rails.root.join('app/assets/docs/Datenschutz_Fairmondo_FINAL.pdf'))
-
-    @token = token
-    devise_mail(record, :welcome_mail)
-  end
-
-  def confirmation_instructions(record, token, opts = {})
-    attachments['AGB_Fairmondo.pdf'] = File.read(Rails.root.join('app/assets/docs/AGB.pdf'))
-    attachments['Datenschutz_Fairmondo.pdf'] = File.read(Rails.root.join('app/assets/docs/Datenschutz_Fairmondo_FINAL.pdf'))
-
-    super
-  end
 end
