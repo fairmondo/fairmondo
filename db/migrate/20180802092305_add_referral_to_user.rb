c9bdffdbf57b5f8ef7025ab6b7b906eb573f343b
hesslau <henrik@hesslau.org>
2018-08-02 11:40:55 +0200

    use empty string as default instead of false, which gets written to database as '0'

--- db/migrate/20180802092305_add_referral_to_user.rb
+++ db/migrate/20180802092305_add_referral_to_user.rb
@@ -1,5 +1,5 @@
 class AddReferralToUser < ActiveRecord::Migration
   def change
-    add_column :users, :referral, :text, :default => false
+    add_column :users, :referral, :text, :default => ""
   end
 end
