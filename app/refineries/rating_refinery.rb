d619bf40d41b9eac7e9bd4dd16fca9ec0cb8bff9
Julian Dobmann <julian@bitcrowd.net>
2020-02-06 15:12:46 +0100

    Remove rating refinery

--- app/refineries/rating_refinery.rb
+++
@@ -1,9 +0,0 @@
-#   Copyright (c) 2012-2017, Fairmondo eG.  This file is
-#   licensed under the GNU Affero General Public License version 3 or later.
-#   See the COPYRIGHT file for details.
-
-class RatingRefinery < ApplicationRefinery
-  def default
-    [:rating, :rated_user_id, :text, :line_item_group_id]
-  end
-end
