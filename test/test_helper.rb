f6265baf197ff6d8c5bcfc4412f1e97dba327056
Andreas Knöpfle <andi@bitcrowd.net>
2020-02-11 11:44:13 +0100

    Repair coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,28 @@
 
 ENV["RAILS_ENV"] = "test"
 
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
+SimpleCov::Formatter::HTMLFormatter,
+Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start 'rails' do
+  add_filter "lib/tasks/*"
+  add_filter "app/models/statistic.rb" #only backend
+  add_filter "app/helpers/statistic_helper.rb" #only backend
+  add_filter "lib/autoload/sidekiq_redis_connection_wrapper.rb" #util-class
+  add_filter "lib/autoload/paperclip_orphan_file_cleaner.rb" #part of rake task
+  #reference implementations
+  add_filter "lib/autoload/paypal_ipn.rb"
+  add_filter "lib/autoload/single_sign_on.rb"
+  minimum_coverage 98
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 
-require Rails.root.join('test/support/spec_helpers/coverage.rb')
 require "rails/test_help"
 require "mocha/minitest"
 
