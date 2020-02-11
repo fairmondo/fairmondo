54d6131514ca01b6834b3d04c594560ef3d3c012
Andreas Knöpfle <andi@bitcrowd.net>
2020-02-11 18:24:49 +0100

    Capistrano bundler changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 #   See the COPYRIGHT file for details.
 
 # config valid only for Capistrano 3.1
-lock '3.3.5'
+lock '3.11.2'
 
 set :application, 'fairnopoly'
 set :repo_url, 'git://github.com/fairnopoly/fairnopoly.git'
@@ -16,9 +16,6 @@ set :rbenv_roles, :all # default value
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Default value for :scm is :git
-set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
