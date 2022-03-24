700c8663bb0a824720534a38119b5420841f9f81
marcus78 <marcus78@mailbox.org>
2022-03-24 13:06:32 +0100

    fuegt Fastbillworker wegen moegliche Abhaengigkeiten bei refund wieder hinzu

---
+++ app/workers/fastbill_update_user_worker.rb
@@ -0,0 +1,17 @@
+#   Copyright (c) 2012-2017, Fairmondo eG.  This file is
+#   licensed under the GNU Affero General Public License version 3 or later.
+#   See the COPYRIGHT file for details.
+
+class FastbillUpdateUserWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :fastbill,
+                  retry: 20,
+                  backtrace: true
+
+  def perform user_id
+    user = User.find(user_id)
+    api = FastbillAPI.new
+    api.update_profile(user)
+  end
+end
